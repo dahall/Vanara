acf3f8c306f6062f382c339077b73f9583c690be
David Hall <dahall68@gmail.com>
2018-03-26 15:01:36 -0600

    Removed ForEach method as it can be accomplished with AsEnumerable.  Added TreeView.EnumerateAllNodes and TreeView.AddSystemItemAsNode methods.

--- WIndows.Forms/Extensions/TreeViewExtension.cs
+++ WIndows.Forms/Extensions/TreeViewExtension.cs
@@ -1,42 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using Vanara.PInvoke;
 using static Vanara.PInvoke.ComCtl32;
+using static Vanara.PInvoke.Shell32;
 using static Vanara.PInvoke.User32_Gdi;
 
 namespace Vanara.Extensions
 {
+	/// <summary>Extension methods for <see cref="TreeView"/> controls.</summary>
 	public static partial class TreeViewExtension
 	{
-		private const int TVM_SETEXTENDEDSTYLE = 0x1100 + 44;
-		private const int TVS_EX_FADEINOUTEXPANDOS = 0x0040;
-		private const int TVS_EX_AUTOHSCROLL = 0x0020;
-		private const int TVS_NOHSCROLL = 0x8000;
-
+		/// <summary>Sets the explorer theme on this <see cref="TreeView"/> instance.</summary>
+		/// <param name="treeView">The tree view on which to set the theme.</param>
+		/// <param name="on">If set to <c>true</c> use the Windows Explorer theme, otherwise set to default theme.</param>
 		public static void SetExplorerTheme(this TreeView treeView, bool on = true)
 		{
-			if (System.Environment.OSVersion.Version.Major >= 6)
+			if (Environment.OSVersion.Version.Major >= 6)
 			{
 				// Make sure the TVS_NOHSCROLL style is set
-				treeView.SetStyle(TVS_NOHSCROLL);
+				treeView.SetStyle((int)TreeViewStyle.TVS_NOHSCROLL);
 
 				// Set explorer theme, set critical properties, and set extended styles
 				treeView.SetWindowTheme(on ? "explorer" : null);
 				if (!on) return;
 				treeView.HotTracking = true;
 				treeView.ShowLines = false;
-				SendMessage(new HandleRef(treeView, treeView.Handle), TVM_SETEXTENDEDSTYLE, TVS_EX_FADEINOUTEXPANDOS | TVS_EX_AUTOHSCROLL, TVS_EX_FADEINOUTEXPANDOS | TVS_EX_AUTOHSCROLL);
+				SendMessage(new HandleRef(treeView, treeView.Handle), (int)TreeViewMessage.TVM_SETEXTENDEDSTYLE, (int)(TreeViewStyleEx.TVS_EX_FADEINOUTEXPANDOS | TreeViewStyleEx.TVS_EX_AUTOHSCROLL), (int)(TreeViewStyleEx.TVS_EX_FADEINOUTEXPANDOS | TreeViewStyleEx.TVS_EX_AUTOHSCROLL));
 			}
 		}
 
-		public static void ForEach(this TreeNodeCollection nodes, Action<TreeNode> action, bool forAllChildren = false)
-		{
-			Action<TreeNode> traverse = null;
-			traverse = n => { action(n); if (forAllChildren) foreach (TreeNode n1 in n.Nodes) traverse(n1); };
-			foreach (TreeNode n in nodes) traverse(n);
-		}
-
+		/// <summary>Enumerates child nodes of a given <see cref="TreeNodeCollection"/>.</summary>
+		/// <param name="nodes">The <see cref="TreeNodeCollection"/> instance from which to start the enumeration.</param>
+		/// <param name="forAllChildren">if set to <c>true</c> also includes child nodes of all child nodes.</param>
+		/// <returns>An <see cref="IEnumerable{T}"/> of all <see cref="TreeNode"/> instances in the collection.</returns>
 		public static IEnumerable<TreeNode> AsEnumerable(this TreeNodeCollection nodes, bool forAllChildren = false)
 		{
 			foreach (TreeNode item in nodes)
@@ -45,6 +45,59 @@ namespace Vanara.Extensions
 				if (forAllChildren)
 					foreach (var child in item.Nodes.AsEnumerable(true))
 						yield return child;
+			}
+		}
+
+		/// <summary>Adds the file system item into a <see cref="TreeView"/> and adds the shell icon associated with it to the <see cref="ImageList"/> of the <c>TreeView</c>.</summary>
+		/// <param name="tv">The tree view into which to add the item.</param>
+		/// <param name="nodes">The <see cref="TreeNodeCollection"/> that receives the newly created <see cref="TreeNode"/>.</param>
+		/// <param name="systemItemPath">The path of the item to add.</param>
+		/// <returns>A <see cref="TreeNode"/> instance created </returns>
+		public static TreeNode AddSystemItemAsNode(this TreeView tv, TreeNodeCollection nodes, string systemItemPath)
+		{
+			var sz = tv.ImageList.ImageSize;
+
+			var ext = Path.GetExtension(systemItemPath);
+			if (string.IsNullOrEmpty(ext))
+				ext = "5EEB255733234c4dBECF9A128E896A1E";
+			//ext = fullName.EndsWith("\\") ? "5EEB255733234c4dBECF9A128E896A1E" : "F9EB930C78D2477c80A51945D505E9C4";
+			else if (ext.Equals(".exe", StringComparison.InvariantCultureIgnoreCase) || ext.Equals(".lnk", StringComparison.InvariantCultureIgnoreCase))
+				ext = Path.GetFileName(systemItemPath);
+
+			if (!tv.ImageList.Images.ContainsKey(ext))
+			{
+				try
+				{
+					tv.ImageList.Images.Add(ext, GetIcon(ext, sz.Height == 16));
+				}
+				catch (ArgumentException ex)
+				{
+					throw new ArgumentException($"File \"{systemItemPath}\" does" + " not exist!", ex);
+				}
+			}
+			return nodes.Add(systemItemPath, Path.GetFileName(systemItemPath), ext, ext);
+		}
+
+		/// <summary>Enumerates all the nodes in the <see cref="TreeView"/>.</summary>
+		/// <param name="treeView">The tree view.</param>
+		/// <returns>An <see cref="IEnumerable{T}"/> of all <see cref="TreeNode"/> instances in this <see cref="TreeView"/>.</returns>
+		public static IEnumerable<TreeNode> EnumerateAllNodes(this TreeView treeView) => AsEnumerable(treeView.Nodes, true);
+
+		private static Icon GetIcon(string path, bool small)
+		{
+			var flags = SHGFI.SHGFI_ICON | SHGFI.SHGFI_USEFILEATTRIBUTES | (small ? SHGFI.SHGFI_SMALLICON : SHGFI.SHGFI_LARGEICON);
+			var attribute = path == "5EEB255733234c4dBECF9A128E896A1E" ? FileAttributes.Directory : FileAttributes.Normal;
+			var shfi = new SHFILEINFO();
+			var res = SHGetFileInfo(path, attribute, ref shfi, Marshal.SizeOf(shfi), flags);
+			if (res == IntPtr.Zero) Win32Error.ThrowLastError();
+			try
+			{
+				Icon.FromHandle(shfi.hIcon);
+				return (Icon)Icon.FromHandle(shfi.hIcon).Clone();
+			}
+			finally
+			{
+				DestroyIcon(shfi.hIcon);
 			}
 		}
 	}
