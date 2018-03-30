80a915f06e33ebf910af3c9f328bc328667f9bb7
David Hall <dahall68@gmail.com>
2018-03-30 11:53:21 -0600

    Added implicit conversion from T and object constructor that casts to T

--- Core/InteropServices/ComReleaser.cs
+++ Core/InteropServices/ComReleaser.cs
@@ -14,9 +14,20 @@ namespace Vanara.InteropServices
 		/// <exception cref="ArgumentException">Argument value must be a COM object. - obj</exception>
 		public ComReleaser(T obj)
 		{
-			if (obj == null) throw new ArgumentNullException("obj");
+			if (obj == null) throw new ArgumentNullException(nameof(obj));
 			if (!obj.GetType().IsCOMObject) throw new ArgumentException("Argument value must be a COM object.", nameof(obj));
 			Item = obj;
+		}
+
+		/// <summary>Initializes a new instance of the <see cref="ComReleaser{T}"/> class.</summary>
+		/// <param name="obj">The COM object instance.</param>
+		/// <exception cref="ArgumentNullException">obj</exception>
+		/// <exception cref="ArgumentException">Argument value must be a COM object and expose <typeparamref name="T"/>.</exception>
+		public ComReleaser(object obj)
+		{
+			if (obj == null) throw new ArgumentNullException(nameof(obj));
+			if (!obj.GetType().IsCOMObject) throw new ArgumentException("Argument value must be a COM object.", nameof(obj));
+			Item = obj is T t ? t : throw new ArgumentException($"Object must expose the {typeof(T).Name} interface.");
 		}
 
 		/// <summary>Gets the COM object.</summary>
@@ -35,5 +46,10 @@ namespace Vanara.InteropServices
 		/// <param name="co">The <see cref="ComReleaser{T}"/> instance.</param>
 		/// <returns>The result of the conversion.</returns>
 		public static implicit operator T(ComReleaser<T> co) => co.Item;
+
+		/// <summary>Performs an implicit conversion from <see cref="T"/> to <see cref="ComReleaser{T}"/>.</summary>
+		/// <param name="obj">The COM object.</param>
+		/// <returns>The result of the conversion.</returns>
+		public static implicit operator ComReleaser<T>(T obj) => new ComReleaser<T>(obj);
 	}
 }
