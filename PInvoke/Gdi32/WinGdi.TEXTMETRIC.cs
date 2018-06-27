c42442badcd1cb87c3123bbe8cb5be349a4c5153
David Hall <dahall68@gmail.com>
2018-06-27 13:04:20 -0600

    Moved LOGFONT to PInvoke.Shared and added GetDeviceCaps

--- PInvoke/Gdi32/WinGdi.TEXTMETRIC.cs
+++ PInvoke/Gdi32/WinGdi.TEXTMETRIC.cs
@@ -7,29 +7,6 @@ namespace Vanara.PInvoke
 {
 	public static partial class Gdi32
 	{
-		/// <summary>Specifies information about the pitch, the technology, and the family of a physical font.</summary>
-		[PInvokeData("Wingdi.h", MSDNShortId = "dd145037")]
-		[Flags]
-		public enum FontPitch : byte
-		{
-			/// <summary>The default pitch, which is implementation-dependent.</summary>
-			DEFAULT_PITCH = 0,
-			/// <summary>A fixed pitch, which means that all the characters in the font occupy the same width when output in a string.</summary>
-			FIXED_PITCH = 1,
-			/// <summary>A variable pitch, which means that the characters in the font occupy widths that are proportional to the actual widths of the glyphs when output in a string. For example, the "i" and space characters usually have much smaller widths than a "W" or "O" character.</summary>
-			VARIABLE_PITCH = 2,
-			/// <summary>The mono font/</summary>
-			MONO_FONT = 8,
-			/// <summary>If this bit is set the font is a variable pitch font. If this bit is clear the font is a fixed pitch font. Note very carefully that those meanings are the opposite of what the constant name implies.</summary>
-			TMPF_FIXED_PITCH = 0x01,
-			/// <summary>If this bit is set the font is a vector font.</summary>
-			TMPF_VECTOR = 0x02,
-			/// <summary>If this bit is set the font is a TrueType font.</summary>
-			TMPF_TRUETYPE = 0x04,
-			/// <summary>If this bit is set the font is a device font.</summary>
-			TMPF_DEVICE = 0x08,
-		}
-
 		public enum TMCHARSET : byte
 		{
 			/// <summary>The ANSI CHARSET</summary>
--- PInvoke/Gdi32/WinGdi.cs
+++ PInvoke/Gdi32/WinGdi.cs
@@ -46,6 +46,94 @@ namespace Vanara.PInvoke
 			LAYOUT_BITMAPORIENTATIONPRESERVED = 8,
 		}
 
+		/// <summary>Values used by the <see cref="GetDeviceCaps"/> function.</summary>
+		public enum DeviceCap
+		{
+			/// <summary>Device driver version</summary>
+			DRIVERVERSION = 0,
+			/// <summary>Device classification</summary>
+			TECHNOLOGY = 2,
+			/// <summary>Horizontal size in millimeters</summary>
+			HORZSIZE = 4,
+			/// <summary>Vertical size in millimeters</summary>
+			VERTSIZE = 6,
+			/// <summary>Horizontal width in pixels</summary>
+			HORZRES = 8,
+			/// <summary>Vertical height in pixels</summary>
+			VERTRES = 10,
+			/// <summary>Number of bits per pixel</summary>
+			BITSPIXEL = 12,
+			/// <summary>Number of planes</summary>
+			PLANES = 14,
+			/// <summary>Number of brushes the device has</summary>
+			NUMBRUSHES = 16,
+			/// <summary>Number of pens the device has</summary>
+			NUMPENS = 18,
+			/// <summary>Number of markers the device has</summary>
+			NUMMARKERS = 20,
+			/// <summary>Number of fonts the device has</summary>
+			NUMFONTS = 22,
+			/// <summary>Number of colors the device supports</summary>
+			NUMCOLORS = 24,
+			/// <summary>Size required for device descriptor</summary>
+			PDEVICESIZE = 26,
+			/// <summary>Curve capabilities</summary>
+			CURVECAPS = 28,
+			/// <summary>Line capabilities</summary>
+			LINECAPS = 30,
+			/// <summary>Polygonal capabilities</summary>
+			POLYGONALCAPS = 32,
+			/// <summary>Text capabilities</summary>
+			TEXTCAPS = 34,
+			/// <summary>Clipping capabilities</summary>
+			CLIPCAPS = 36,
+			/// <summary>Bitblt capabilities</summary>
+			RASTERCAPS = 38,
+			/// <summary>Length of the X leg</summary>
+			ASPECTX = 40,
+			/// <summary>Length of the Y leg</summary>
+			ASPECTY = 42,
+			/// <summary>Length of the hypotenuse</summary>
+			ASPECTXY = 44,
+			/// <summary>Shading and Blending caps</summary>
+			SHADEBLENDCAPS = 45,
+
+			/// <summary>Logical pixels inch in X</summary>
+			LOGPIXELSX = 88,
+			/// <summary>Logical pixels inch in Y</summary>
+			LOGPIXELSY = 90,
+
+			/// <summary>Number of entries in physical palette</summary>
+			SIZEPALETTE = 104,
+			/// <summary>Number of reserved entries in palette</summary>
+			NUMRESERVED = 106,
+			/// <summary>Actual color resolution</summary>
+			COLORRES = 108,
+
+			// Printing related DeviceCaps. These replace the appropriate Escapes
+			/// <summary>Physical Width in device units</summary>
+			PHYSICALWIDTH = 110,
+			/// <summary>Physical Height in device units</summary>
+			PHYSICALHEIGHT = 111,
+			/// <summary>Physical Printable Area x margin</summary>
+			PHYSICALOFFSETX = 112,
+			/// <summary>Physical Printable Area y margin</summary>
+			PHYSICALOFFSETY = 113,
+			/// <summary>Scaling factor x</summary>
+			SCALINGFACTORX = 114,
+			/// <summary>Scaling factor y</summary>
+			SCALINGFACTORY = 115,
+
+			/// <summary>Current vertical refresh rate of the display device (for displays only) in Hz</summary>
+			VREFRESH = 116,
+			/// <summary>Vertical height of entire desktop in pixels</summary>
+			DESKTOPVERTRES = 117,
+			/// <summary>Horizontal width of entire desktop in pixels</summary>
+			DESKTOPHORZRES = 118,
+			/// <summary>Preferred blt alignment</summary>
+			BLTALIGNMENT = 119
+		}
+		
 		/// <summary>
 		/// Defines how the color data for the source rectangle is to be combined with the color data for the destination rectangle to achieve the final color
 		/// when using the <see cref="BitBlt"/> function.
@@ -288,6 +376,15 @@ namespace Vanara.PInvoke
 		[System.Security.SecurityCritical]
 		[PInvokeData("Wingdi.h", MSDNShortId = "dd144844")]
 		public static extern bool GdiFlush();
+
+		/// <summary>The GetDeviceCaps function retrieves device-specific information for the specified device.</summary>
+		/// <param name="hdc">A handle to the DC.</param>
+		/// <param name="index">The item to be returned.</param>
+		/// <returns>The return value specifies the value of the desired item. When nIndex is BITSPIXEL and the device has 15bpp or 16bpp, the return value is 16.</returns>
+		// https://msdn.microsoft.com/en-us/library/windows/desktop/dd144877(v=vs.85).aspx
+		[DllImport(Lib.Gdi32, SetLastError = false, ExactSpelling = true)]
+		[PInvokeData("Wingdi.h", MSDNShortId = "dd144877")]
+		public static extern int GetDeviceCaps(IntPtr hdc, DeviceCap index);
 
 		/// <summary>The GetObject function retrieves information for the specified graphics object.</summary>
 		/// <param name="hgdiobj">
