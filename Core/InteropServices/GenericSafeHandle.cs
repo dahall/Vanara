69acd1fecee02447fd534881e588b91a4ef5bcd7
David Hall <dahall@byu.net>
2018-08-10 09:46:42 -0600

    Added more open constructor for derived classes.

--- Core/InteropServices/GenericSafeHandle.cs
+++ Core/InteropServices/GenericSafeHandle.cs
@@ -12,13 +12,18 @@ namespace Vanara.InteropServices
 		protected GenericSafeHandle() : base(true) { }
 
 		/// <summary>Initializes a new instance of the <see cref="GenericSafeHandle"/> class.</summary>
+		/// <param name="ptr">The pre-existing handle to use.</param>
+		/// <param name="ownsHandle"><see langword="true"/> to reliably release the handle during the finalization phase; <see langword="false"/> to prevent reliable release (not recommended).</param>
+		protected GenericSafeHandle(IntPtr ptr, bool ownsHandle) : base(ownsHandle) { SetHandle(ptr); }
+
+		/// <summary>Initializes a new instance of the <see cref="GenericSafeHandle"/> class.</summary>
 		/// <param name="closeMethod">The delegate method for closing the handle.</param>
 		public GenericSafeHandle(Func<IntPtr, bool> closeMethod) : this(IntPtr.Zero, closeMethod, true) { }
 
 		/// <summary>Initializes a new instance of the <see cref="GenericSafeHandle"/> class.</summary>
-		/// <param name="ptr">The PTR.</param>
+		/// <param name="ptr">The pre-existing handle to use.</param>
 		/// <param name="closeMethod">The delegate method for closing the handle.</param>
-		/// <param name="ownsHandle">if set to <c>true</c> [owns handle].</param>
+		/// <param name="ownsHandle"><see langword="true"/> to reliably release the handle during the finalization phase; <see langword="false"/> to prevent reliable release (not recommended).</param>
 		/// <exception cref="System.ArgumentNullException">closeMethod</exception>
 		public GenericSafeHandle(IntPtr ptr, Func<IntPtr, bool> closeMethod, bool ownsHandle = true) : base(ownsHandle)
 		{
