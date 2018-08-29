180a010d96f21f54dbdeacbe1527c743358d0a37
David Hall <dahall@byu.net>
2018-08-29 12:56:26 -0600

    Added NTDLL project

---
+++ PInvoke/NTDll/NTDll.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Vanara.PInvoke
+{
+	/// <summary>Platform invokable enumerated types, constants and functions from ntdll.h</summary>
+	public static partial class NTDll
+	{
+	}
+}
