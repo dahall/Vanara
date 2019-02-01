08f46aba3149038261ddd0c00c9014212b43dbfc
David Hall <dahall@byu.net>
2019-02-01 11:38:58 -0700

    Tried out some test methods for SHCreateShellItemArray

---
+++ UnitTests/PInvoke/Shell32/Methods.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using static Vanara.PInvoke.Shell32;
+
+namespace Vanara.PInvoke.Tests
+{
+	[TestFixture]
+	public class Shell32Methods
+	{
+		// [Test]
+		public void SHCreateShellItemArrayTest()
+		{
+		}
+	}
+}
