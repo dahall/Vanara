7164c624b1f0b192082fba34176c9b426cf89a4f
David Hall <dahall@byu.net>
2019-04-10 13:10:35 -0600

    Added missing using statement

--- PInvoke/Security/AdvApi32/PSID.cs
+++ PInvoke/Security/AdvApi32/PSID.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices;
 using Vanara.Extensions;
 using Vanara.InteropServices;
