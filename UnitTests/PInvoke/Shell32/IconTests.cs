49f249104a67389f6df4c794cd5325d56a306179
David Hall <dahall@byu.net>
2019-06-10 15:30:03 -0600

    Added some Shell32 method tests

---
+++ UnitTests/PInvoke/Shell32/IconTests.cs
@@ -0,0 +1,150 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using static Vanara.PInvoke.Kernel32;
+using static Vanara.PInvoke.Shell32;
+using static Vanara.PInvoke.User32_Gdi;
+
+namespace Vanara.PInvoke.Tests
+{
+	[TestFixture]
+	public class Shell32_IconTests
+	{
+		[Test]
+		public void ExtractAssociatedIconTest()
+		{
+			const string icoFile = @"C:\Temp\Test.docx";
+			var sb = new StringBuilder(icoFile, MAX_PATH);
+			ushort iidx = 0;
+			using (var hIcon = ExtractAssociatedIcon(HINSTANCE.NULL, sb, ref iidx))
+				Assert.That(hIcon.IsInvalid, Is.False);
+			TestContext.WriteLine(sb);
+		}
+
+		[Test]
+		public void ExtractAssociatedIconExTest()
+		{
+			const string icoFile = @"C:\Temp\Test.docx";
+			var sb = new StringBuilder(icoFile, MAX_PATH);
+			ushort iidx = 0, iid = 0;
+			using (var hIcon = ExtractAssociatedIconEx(HINSTANCE.NULL, sb, ref iidx, ref iid))
+			{
+				Assert.That(hIcon.IsInvalid, Is.False);
+				TestContext.WriteLine($"{sb}:{iidx}:{iid}");
+
+				using (var hDupIcon = DuplicateIcon(HINSTANCE.NULL, hIcon))
+					Assert.That(hDupIcon.IsInvalid, Is.False);
+			}
+		}
+
+		[Test]
+		public void ExtractIconTest()
+		{
+			using (var ico1 = ExtractIcon(HINSTANCE.NULL, "notepad.exe", 0))
+				Assert.That(ico1.IsInvalid, Is.False);
+			using (var ico2 = ExtractIcon(HINSTANCE.NULL, "notepad.exe", -2))
+				Assert.That(ico2.IsInvalid, Is.False);
+			using (var icoCnt = ExtractIcon(HINSTANCE.NULL, "notepad.exe", -1))
+				Assert.That(icoCnt.DangerousGetHandle().ToInt32(), Is.EqualTo(1));
+		}
+
+		[Test]
+		public void ExtractIconExTest()
+		{
+			var smIco = new[] { new HICON(IntPtr.Zero) };
+			var lgIco = new[] { new HICON(IntPtr.Zero) };
+			var ico1 = ExtractIconEx("notepad.exe", 0, lgIco, smIco, 1);
+			Assert.That(ico1, Is.EqualTo(2));
+			Free();
+			var ico2 = ExtractIconEx("notepad.exe", -2, lgIco, smIco, 1);
+			Assert.That(ico2, Is.EqualTo(2));
+			Free();
+			var icoCnt = ExtractIconEx("notepad.exe", -1, null, null, 0);
+			Assert.That(icoCnt, Is.EqualTo(1));
+
+			void Free()
+			{
+				DestroyIcon(smIco[0]);
+				DestroyIcon(lgIco[0]);
+			}
+		}
+
+		[Test]
+		public void ExtractIconExTest2()
+		{
+			var ico1 = ExtractIconEx("notepad.exe", 0, 1, out var lgIco, out var smIco);
+			Assert.That(ico1, Is.EqualTo(2));
+			Assert.That(lgIco, Is.Not.Null.And.Length.EqualTo(1));
+			var ico2 = ExtractIconEx("notepad.exe", -2, 1, out lgIco, out smIco);
+			Assert.That(ico2, Is.EqualTo(2));
+			var icoCnt = ExtractIconEx("notepad.exe", -1, 0, out lgIco, out smIco);
+			Assert.That(icoCnt, Is.EqualTo(1));
+			Assert.That(lgIco, Is.Null);
+		}
+
+		[Test]
+		public void PickIconDlgTest()
+		{
+			var sb = new StringBuilder("moricons.dll", MAX_PATH);
+			var idx = 0;
+			Assert.That(PickIconDlg(HWND.NULL, sb, (uint)sb.Length + 1, ref idx), Is.True);
+		}
+
+		[Test]
+		public void SHCreateDefaultExtractIconTest()
+		{
+			Assert.That(SHCreateDefaultExtractIcon(typeof(IDefaultExtractIconInit).GUID, out var ppv), Is.EqualTo((HRESULT)0));
+			Assert.That(ppv, Is.Not.Null);
+			Marshal.FinalReleaseComObject(ppv);
+		}
+
+		[Test]
+		public void SHCreateFileExtractIconWTest()
+		{
+			const string icoFile = @"notepad.exe";
+			Assert.That(SHCreateFileExtractIconW(icoFile, FileFlagsAndAttributes.FILE_ATTRIBUTE_NORMAL, typeof(IExtractIcon).GUID, out var ppv), Is.EqualTo((HRESULT)0));
+			Assert.That(ppv, Is.Not.Null);
+			((IExtractIcon)ppv).Extract(icoFile, 0, out var lg, out var sm, Macros.MAKELONG(48, 16)).ThrowIfFailed();
+			Assert.That(sm.IsInvalid, Is.False);
+			Assert.That(sm.ToIcon().Height, Is.EqualTo(16));
+			Marshal.FinalReleaseComObject(ppv);
+		}
+
+		[Test]
+		public void SHDefExtractIconTest()
+		{
+			const string icoFile = @"notepad.exe";
+			Assert.That(SHDefExtractIcon(icoFile, -2, 0, out var lg, out var sm, Macros.MAKELONG(48, 16)), Is.EqualTo((HRESULT)0));
+			Assert.That(sm.IsInvalid, Is.False);
+			Assert.That(sm.ToIcon().Height, Is.EqualTo(16));
+		}
+
+		[Test]
+		public void SHGetIconOverlayIndexTest()
+		{
+			Assert.That(SHGetIconOverlayIndex(null, IDO_SHGIOI_LINK), Is.Not.Zero);
+		}
+
+		[Test]
+		public void SHGetStockIconInfoTest()
+		{
+			var psii = new SHSTOCKICONINFO { cbSize = (uint)Marshal.SizeOf(typeof(SHSTOCKICONINFO)) };
+			Assert.That(SHGetStockIconInfo(SHSTOCKICONID.SIID_APPLICATION, SHGSI.SHGSI_ICON, ref psii), Is.EqualTo((HRESULT)0));
+			Assert.That(psii.hIcon.IsNull, Is.False);
+			DestroyIcon(psii.hIcon);
+		}
+
+		[Test]
+		public void SHLoadNonloadedIconOverlayIdentifiersTest()
+		{
+			Assert.That(SHLoadNonloadedIconOverlayIdentifiers(), Is.EqualTo((HRESULT)0));
+		}
+
+		/*
+		Shell_NotifyIcon
+		Shell_NotifyIconGetRect
+		*/
+	}
+}
--- UnitTests/PInvoke/Shell32/Methods.cs
+++ UnitTests/PInvoke/Shell32/Methods.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System.Linq;
 using static Vanara.PInvoke.Shell32;
 
 namespace Vanara.PInvoke.Tests
@@ -6,9 +7,207 @@ namespace Vanara.PInvoke.Tests
 	[TestFixture]
 	public class Shell32Methods
 	{
-		// [Test]
-		public void SHCreateShellItemArrayTest()
+		[Test]
+		public void CommandLineToArgvWTest()
 		{
+			const string cmd = "-d -s \"Str\" --hog";
+			var h = CommandLineToArgvW(cmd, out var c);
+			Assert.That(h.IsInvalid, Is.False);
+			Assert.That(c, Is.EqualTo(4));
+			Assert.That(h.ToStringEnum(c, System.Runtime.InteropServices.CharSet.Unicode), Is.Not.Empty);
+			Assert.That(h.ToStringEnum(c, System.Runtime.InteropServices.CharSet.Unicode).First(), Is.EqualTo("-d"));
+
+			Assert.That(CommandLineToArgvW(cmd).First(), Is.EqualTo("-d"));
+			TestContext.WriteLine(string.Join(" | ", CommandLineToArgvW(cmd)));
 		}
+
+		[Test]
+		public void AssocCreateForClassesTest()
+		{
+			Assert.Fail("Not implemented.");
+			//Assert.That(AssocCreateForClasses(), Is.Zero);
+		}
+
+		[Test]
+		public void AssocGetDetailsOfPropKeyTest()
+		{
+			Assert.Fail("Not implemented.");
+			//Assert.That(AssocGetDetailsOfPropKey(), Is.Zero);
+		}
+
+		/*
+		AssocCreateForClasses
+		AssocGetDetailsOfPropKey
+		CDefFolderMenu_Create2
+		DragAcceptFiles
+		DragFinish
+		DragQueryFile
+		DragQueryPoint
+		DuplicateIcon
+		ExtractAssociatedIcon
+		ExtractAssociatedIconEx
+		ExtractIcon
+		ExtractIconEx
+		FindExecutable
+		GetCurrentProcessExplicitAppUserModelID
+		GetFileNameFromBrowse
+		InitNetworkAddressControl
+		IsNetDrive
+		IsUserAnAdmin
+		OpenRegStream
+		PathCleanupSpec
+		PathGetShortPath
+		PathIsExe
+		PathIsSlow
+		PathMakeUniqueName
+		PathResolve
+		PathYetAnotherMakeUniqueName
+		PickIconDlg
+		PifMgr_CloseProperties
+		PifMgr_GetProperties
+		PifMgr_OpenProperties
+		PifMgr_SetProperties
+		ReadCabinetState
+		RealDriveType
+		RestartDialog
+		RestartDialogEx
+		SetCurrentProcessExplicitAppUserModelID
+		SHAddDefaultPropertiesByExt
+		SHAddFromPropSheetExtArray
+		SHAddToRecentDocs
+		SHAlloc
+		SHAppBarMessage
+		SHAssocEnumHandlers
+		SHAssocEnumHandlersForProtocolByApplication
+		SHBindToFolderIDListParent
+		SHBindToFolderIDListParentEx
+		SHBindToObject
+		SHBindToParent
+		SHBrowseForFolder
+		SHChangeNotification_Lock
+		SHChangeNotification_Unlock
+		SHChangeNotify
+		SHChangeNotifyDeregister
+		SHChangeNotifyRegister
+		SHChangeNotifyRegisterThread
+		SHCreateAssociationRegistration
+		SHCreateDataObject
+		SHCreateDefaultContextMenu
+		SHCreateDefaultExtractIcon
+		SHCreateDefaultPropertiesOp
+		SHCreateDirectory
+		SHCreateDirectoryEx
+		SHCreateFileExtractIconW
+		SHCreateItemFromIDList
+		SHCreateItemFromParsingName
+		SHCreateItemFromRelativeName
+		SHCreateItemInKnownFolder
+		SHCreateItemWithParent
+		SHCreatePropSheetExtArray
+		SHCreateShellFolderView
+		SHCreateShellFolderViewEx
+		SHCreateShellItem
+		SHCreateShellItemArray
+		SHCreateShellItemArrayFromDataObject
+		SHCreateShellItemArrayFromIDLists
+		SHCreateShellItemArrayFromShellItem
+		SHCreateStdEnumFmtEtc
+		SHDefExtractIcon
+		SHDestroyPropSheetExtArray
+		SHDoDragDrop
+		Shell_GetCachedImageIndex
+		Shell_GetImageLists
+		Shell_MergeMenus
+		Shell_NotifyIcon
+		Shell_NotifyIconGetRect
+		ShellAbout
+		ShellExecute
+		ShellExecuteEx
+		SHEmptyRecycleBin
+		SHEnumerateUnreadMailAccountsA
+		SHEnumerateUnreadMailAccountsW
+		SHEvaluateSystemCommandTemplate
+		SHFileOperation
+		SHFind_InitMenuPopup
+		SHFindFiles
+		SHFlushSFCache
+		SHFormatDrive
+		SHFree
+		SHFreeNameMappings
+		SHGetAttributesFromDataObject
+		SHGetDataFromIDList
+		SHGetDesktopFolder
+		SHGetDiskFreeSpaceA
+		SHGetDiskFreeSpaceEx
+		SHGetDiskFreeSpaceW
+		SHGetDriveMedia
+		SHGetFileInfo
+		SHGetFolderLocation
+		SHGetFolderPath
+		SHGetFolderPathAndSubDir
+		SHGetFolderPathEx
+		SHGetIconOverlayIndex
+		SHGetIDListFromObject
+		SHGetImageList
+		SHGetInstanceExplorer
+		SHGetItemFromDataObject
+		SHGetItemFromObject
+		SHGetKnownFolderIDList
+		SHGetKnownFolderItem
+		SHGetKnownFolderPath
+		SHGetLocalizedName
+		SHGetNameFromIDList
+		SHGetNewLinkInfo
+		SHGetPathFromIDList
+		SHGetPathFromIDListEx
+		SHGetPropertyStoreForWindow
+		SHGetPropertyStoreFromIDList
+		SHGetPropertyStoreFromParsingName
+		SHGetRealIDL
+		SHGetSetFolderCustomSettings
+		SHGetSetSettings
+		SHGetSettings
+		SHGetStockIconInfo
+		SHGetTemporaryPropertyForItem
+		SHGetUnreadMailCountW
+		SHHandleUpdateImage
+		SHInvokePrinterCommand
+		SHIsFileAvailableOffline
+		SHLimitInputEdit
+		SHLoadInProc
+		SHLoadNonloadedIconOverlayIdentifiers
+		SHMapPIDLToSystemImageListIndex
+		SHMultiFileProperties
+		SHObjectProperties
+		SHOpenFolderAndSelectItems
+		SHOpenWithDialog
+		SHParseDisplayName
+		SHPathPrepareForWrite
+		SHPropStgCreate
+		SHPropStgReadMultiple
+		SHPropStgWriteMultiple
+		SHQueryRecycleBin
+		SHQueryUserNotificationState
+		SHRemoveLocalizedName
+		SHReplaceFromPropSheetExtArray
+		SHResolveLibrary
+		SHRestricted
+		SHSetDefaultProperties
+		SHSetInstanceExplorer
+		SHSetKnownFolderPath
+		SHSetLocalizedName
+		SHSetTemporaryPropertyForItem
+		SHSetUnreadMailCountW
+		SHShellFolderView_Message
+		SHShowManageLibraryUI
+		SHSimpleIDListFromPath
+		SHTestTokenMembership
+		SHUpdateImage
+		SHValidateUNC
+		SignalFileOpen
+		StgMakeUniqueName
+		Win32DeleteFile
+		WriteCabinetState
+		*/
 	}
 }
