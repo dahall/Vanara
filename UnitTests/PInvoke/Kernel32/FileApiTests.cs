6b5cea3719e549a90a9c7c2ecaee99eaa0c1c6fa
David Hall <dahall@byu.net>
2019-06-22 14:19:16 -0600

    Exposed TempFile to all tests in Kernel32

--- UnitTests/PInvoke/Kernel32/FileApiTests.cs
+++ UnitTests/PInvoke/Kernel32/FileApiTests.cs
@@ -453,28 +453,28 @@ namespace Vanara.PInvoke.Tests
 				Assert.That(b, Is.True);
 			}
 		}
+	}
 
-		internal class TempFile : IDisposable
+	internal class TempFile : IDisposable
+	{
+		public const string tmpstr = @"Temporary";
+
+		public TempFile(Kernel32.FileAccess dwDesiredAccess, FileShare dwShareMode, FileMode dwCreationDisposition = FileMode.OpenOrCreate, FileFlagsAndAttributes dwFlagsAndAttributes = FileFlagsAndAttributes.FILE_ATTRIBUTE_NORMAL) : this()
 		{
-			public const string tmpstr = @"Temporary";
+			hFile = CreateFile(FullName, dwDesiredAccess, dwShareMode, null, dwCreationDisposition, dwFlagsAndAttributes, IntPtr.Zero);
+		}
 
-			public TempFile(Kernel32.FileAccess dwDesiredAccess, FileShare dwShareMode, FileMode dwCreationDisposition = FileMode.OpenOrCreate, FileFlagsAndAttributes dwFlagsAndAttributes = FileFlagsAndAttributes.FILE_ATTRIBUTE_NORMAL) : this()
-			{
-				hFile = CreateFile(FullName, dwDesiredAccess, dwShareMode, null, dwCreationDisposition, dwFlagsAndAttributes, IntPtr.Zero);
-			}
+		public TempFile(string contents = tmpstr)
+		{
+			FullName = Path.GetTempFileName(); File.WriteAllText(FullName, contents);
+		}
 
-			public TempFile(string contents = tmpstr)
-			{
-				FullName = Path.GetTempFileName(); File.WriteAllText(FullName, contents);
-			}
+		public string FullName { get; }
+		public SafeHFILE hFile { get; }
 
-			public string FullName { get; }
-			public SafeHFILE hFile { get; }
-
-			void IDisposable.Dispose()
-			{
-				hFile?.Dispose(); File.Delete(FullName);
-			}
+		void IDisposable.Dispose()
+		{
+			hFile?.Dispose(); File.Delete(FullName);
 		}
 	}
 }
