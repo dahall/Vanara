c3b56933890cfb929704b834d7058abfb766fd66
David Hall <dahall@byu.net>
2019-08-10 13:42:50 -0600

    Added unit tests for mschapp.h

--- UnitTests/PInvoke/Security/SecurityTests.cs
+++ UnitTests/PInvoke/Security/SecurityTests.cs
@@ -14,5 +14,11 @@ namespace Vanara.PInvoke.Tests
 	[TestFixture()]
 	public class SecurityTests
 	{
+		[Test]
+		public void MSChapSrvChangePasswordTest()
+		{
+			Assert.That(MSChapSrvChangePassword(null, Environment.UserName, false, default, default, default, default), ResultIs.Successful);
+			Assert.That(MSChapSrvChangePassword2(null, Environment.UserName, default, default, false, default, default), ResultIs.Successful);
+		}
 	}
 }
