ae467f5bacc6749efa0e0fe121e30a701e048ea5
David Hall <dahall@byu.net>
2019-08-19 15:06:36 -0600

    Added ability to provide token

--- UnitTests/CSharpRunner/PrivBlock.cs
+++ UnitTests/CSharpRunner/PrivBlock.cs
@@ -10,13 +10,30 @@ namespace Vanara.PInvoke.Tests
 		SafeCoTaskMemHandle prevState;
 		SafeHTOKEN tok;
 
-		public ElevPriv(string priv, HPROCESS hProc = default, TokenAccess access = TokenAccess.TOKEN_ADJUST_PRIVILEGES | TokenAccess.TOKEN_QUERY)
+		public ElevPriv(string priv, HPROCESS hProc = default, TokenAccess access = TokenAccess.TOKEN_ADJUST_PRIVILEGES | TokenAccess.TOKEN_QUERY) :
+			this(new[] { priv }, hProc, access)
+		{ }
+
+		public ElevPriv(string[] privs, HPROCESS hProc = default, TokenAccess access = TokenAccess.TOKEN_ADJUST_PRIVILEGES | TokenAccess.TOKEN_QUERY)
 		{
 			if (hProc.IsNull) hProc = GetCurrentProcess();
 			tok = SafeHTOKEN.FromProcess(hProc, access);
-			var newPriv = new PTOKEN_PRIVILEGES(LUID.FromName(priv), PrivilegeAttributes.SE_PRIVILEGE_ENABLED);
+			ElevatePrivileges(privs);
+		}
+
+		public ElevPriv(string priv, HTOKEN hToken) : this(new[] { priv }, hToken) { }
+
+		public ElevPriv(string[] privs, HTOKEN hToken)
+		{
+			tok = new SafeHTOKEN((IntPtr)hToken, false);
+			ElevatePrivileges(privs);
+		}
+
+		private void ElevatePrivileges(string[] privs)
+		{
+			var newPriv = new PTOKEN_PRIVILEGES(Array.ConvertAll(privs, s => new LUID_AND_ATTRIBUTES(LUID.FromName(s), PrivilegeAttributes.SE_PRIVILEGE_ENABLED)));
 			prevState = PTOKEN_PRIVILEGES.GetAllocatedAndEmptyInstance();
-			if (!AdjustTokenPrivileges(tok, false, newPriv, (uint)prevState.Size, prevState, out var retLen))
+			if (!AdjustTokenPrivileges(tok, false, newPriv, prevState.Size, prevState, out var retLen))
 				Win32Error.ThrowLastError();
 			prevState.Size = (int)retLen;
 		}
