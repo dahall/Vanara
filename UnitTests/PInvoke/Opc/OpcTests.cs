95b1a68a6a35981a2622fb5188dd1a9768dead34
dahall <dahall@byu.net>
2020-01-11 16:18:33 -0700

    Added empty unit test projects for Opc and UrlMon libs

---
+++ UnitTests/PInvoke/Opc/OpcTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using Vanara.Extensions;
+using Vanara.InteropServices;
+using static Vanara.PInvoke.Opc;
+
+namespace Vanara.PInvoke.Tests
+{
+	[TestFixture]
+	public class OpcTests
+	{
+	}
+}
