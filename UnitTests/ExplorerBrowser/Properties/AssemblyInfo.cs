c89c9f317df6a9dc16c499713bba7645b0e0dc26
dahall <dahall@byu.net>
2020-04-15 13:03:53 -0600

    Updated versions to 3.2.6

--- UnitTests/ExplorerBrowser/Properties/AssemblyInfo.cs
+++ UnitTests/ExplorerBrowser/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.2.5.0")]
-[assembly: AssemblyFileVersion("3.2.5.0")]
+[assembly: AssemblyVersion("3.2.6.0")]
+[assembly: AssemblyFileVersion("3.2.6.0")]
