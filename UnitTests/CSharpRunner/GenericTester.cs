fcac2fc38b0ad7b460b09121ffc4c79cefc9f579
dahall <dahall@byu.net>
2020-05-14 10:13:48 -0600

    Added test class harness for testing COM and classes that span all tests

---
+++ UnitTests/CSharpRunner/GenericTester.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Vanara.InteropServices;
+
+namespace Vanara.PInvoke
+{
+	public class GenericComTester<TInt> where TInt : class
+	{
+		protected Stack<object> objects = new Stack<object>();
+
+		public virtual TInt Instance => (TInt)objects.Peek();
+
+		[OneTimeSetUp]
+		public virtual void Setup() => objects.Push(InitInstance());
+
+		[OneTimeTearDown]
+		public virtual void TearDown()
+		{
+			while (objects.Count > 0)
+				Marshal.FinalReleaseComObject(objects.Pop());
+		}
+
+		protected virtual TInt InitInstance() => Activator.CreateInstance<TInt>();
+	}
+
+	public class GenericTester<T> where T : class, IDisposable
+	{
+		public virtual T Instance { get; protected set; }
+
+		[OneTimeSetUp]
+		public virtual void Setup() => Instance = InitInstance();
+
+		[OneTimeTearDown]
+		public virtual void TearDown() => Instance?.Dispose();
+
+		protected virtual T InitInstance() => Activator.CreateInstance<T>();
+	}
+}
