7ff9e77f4b9f704b75e4460f3df40b4261131b74
dahall <dahall@byu.net>
2020-06-01 09:39:40 -0600

    Fixed packing of SHQUERYRBINFO (#127)

--- PInvoke/Shell32/ShellApi.cs
+++ PInvoke/Shell32/ShellApi.cs
@@ -3973,7 +3973,7 @@ namespace Vanara.PInvoke
 		// https://docs.microsoft.com/en-us/windows/desktop/api/shellapi/ns-shellapi-_shqueryrbinfo typedef struct _SHQUERYRBINFO { DWORD
 		// cbSize; __int64 i64Size; __int64 i64NumItems; DWORDLONG i64Size; DWORDLONG i64NumItems; } SHQUERYRBINFO, *LPSHQUERYRBINFO;
 		[PInvokeData("shellapi.h", MSDNShortId = "7e9bc7e9-5712-45e7-a424-0afb62f26450")]
-		[StructLayout(LayoutKind.Sequential)]
+		[StructLayout(LayoutKind.Sequential, Pack = 4)]
 		public struct SHQUERYRBINFO
 		{
 			/// <summary>The size of the structure, in bytes. This member must be filled in prior to calling the function.</summary>
