ff11d2e39382f6795c320be2f8556c345398af32
dahall <dahall@byu.net>
2020-09-25 17:16:22 -0600

    Added standard HWND constants as static fields in HWND

--- PInvoke/Shared/Handles.cs
+++ PInvoke/Shared/Handles.cs
@@ -1926,6 +1926,24 @@ namespace Vanara.PInvoke
 		/// <param name="preexistingHandle">An <see cref="IntPtr"/> object that represents the pre-existing handle to use.</param>
 		public HWND(IntPtr preexistingHandle) => handle = preexistingHandle;
 
+		/// <summary>
+		/// Places the window at the bottom of the Z order. If the hWnd parameter identifies a topmost window, the window loses its
+		/// topmost status and is placed at the bottom of all other windows.
+		/// </summary>
+		public static HWND HWND_BOTTOM = new IntPtr(1);
+
+		/// <summary>
+		/// Places the window above all non-topmost windows (that is, behind all topmost windows). This flag has no effect if the window
+		/// is already a non-topmost window.
+		/// </summary>
+		public static HWND HWND_NOTOPMOST = new IntPtr(-2);
+
+		/// <summary>Places the window at the top of the Z order.</summary>
+		public static HWND HWND_TOP = new IntPtr(0);
+
+		/// <summary>Places the window above all non-topmost windows. The window maintains its topmost position even when it is deactivated.</summary>
+		public static HWND HWND_TOPMOST = new IntPtr(-1);
+
 		/// <summary>Returns an invalid handle by instantiating a <see cref="HWND"/> object with <see cref="IntPtr.Zero"/>.</summary>
 		public static HWND NULL => new HWND(IntPtr.Zero);
 
