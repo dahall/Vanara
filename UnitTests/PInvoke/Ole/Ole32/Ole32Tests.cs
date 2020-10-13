1fb8a2dc8a116995831730a7d433a07f940cc5ef
dahall <dahall@byu.net>
2020-10-12 21:37:15 -0600

    Test for IPropertySetStorage

--- UnitTests/PInvoke/Ole/Ole32/Ole32Tests.cs
+++ UnitTests/PInvoke/Ole/Ole32/Ole32Tests.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.ComTypes;
+using Vanara.InteropServices;
 using static Vanara.PInvoke.Ole32;
 using static Vanara.PInvoke.PropSys;
 
@@ -61,6 +62,38 @@ namespace Vanara.PInvoke.Tests
 		{
 			var m = new STGMEDIUM {tymed = TYMED.TYMED_HGLOBAL, unionmember = Marshal.AllocHGlobal(16)};
 			Assert.That(() => ReleaseStgMedium(m), Throws.Nothing);
+		}
+
+		[Test]
+		public void IPropSetStorageTest()
+		{
+			var propKey = PROPERTYKEY.System.Title;
+
+			// creates a new storage object using NTFS implementation
+			StgOpenStorageEx(TestCaseSources.LogFile, STGM.STGM_DIRECT | STGM.STGM_READWRITE | STGM.STGM_SHARE_EXCLUSIVE,
+				STGFMT.STGFMT_ANY, default, default, default, typeof(IPropertySetStorage).GUID, out var iptr).ThrowIfFailed();
+			using var istg = ComReleaserFactory.Create((IPropertySetStorage)iptr);
+
+			var prc = new PROPSPEC(propKey.Id);
+
+			// creates propertystorage
+			istg.Item.Create(propKey.Key, default, PROPSETFLAG.PROPSETFLAG_DEFAULT, STGM.STGM_CREATE | STGM.STGM_READWRITE | STGM.STGM_SHARE_EXCLUSIVE, out var ipse).ThrowIfFailed();
+			using (var pipse = ComReleaserFactory.Create(ipse))
+			{
+				// write property
+				using var prv = new PROPVARIANT("VALUE", VarEnum.VT_LPWSTR);
+				ipse.WriteMultiple(new[] { prc }, new[] { prv }, PID_FIRST_USABLE).ThrowIfFailed();
+			}
+
+			//hr = ipse.Commit((uint)STGC.STGC_DEFAULT);
+			// read property
+			istg.Item.Open(propKey.Key, STGM.STGM_READ | STGM.STGM_SHARE_EXCLUSIVE, out ipse).ThrowIfFailed();
+			using (var pipse = ComReleaserFactory.Create(ipse))
+			{
+				ipse.ReadMultiple(new[] { prc }, out var prvRead).ThrowIfFailed();
+				Assert.That(prvRead.Length, Is.EqualTo(1));
+				Assert.That(prvRead[0].Value, Is.EqualTo("VALUE"));
+			}
 		}
 	}
 }
