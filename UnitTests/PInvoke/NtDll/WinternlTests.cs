2b1c557f262530f36a9343bb18e6d02f39b4fa4d
dahall <dahall@byu.net>
2021-01-25 12:26:10 -0700

    Updated NtDll unit tests

--- UnitTests/PInvoke/NtDll/WinternlTests.cs
+++ UnitTests/PInvoke/NtDll/WinternlTests.cs
@@ -15,81 +15,55 @@ namespace Vanara.PInvoke.Tests
 		[Test]
 		public void NtQueryInformationProcessTest()
 		{
-			var curProc = Process.GetCurrentProcess();
-			HPROCESS hProc = curProc.Handle;
+			HPROCESS hProc = Kernel32.GetCurrentProcess();
 
-			NtQueryResult<PROCESS_BASIC_INFORMATION> pbi = null;
-			Assert.That(() => pbi = NtQueryInformationProcess<PROCESS_BASIC_INFORMATION>(hProc, PROCESSINFOCLASS.ProcessBasicInformation), Throws.Nothing);
-			Assert.That(pbi, Is.Not.Null);
-			// Can do AsRef here since PROCESS_BASIC_INFORMATION has no managed types
-			ref var rpbi = ref pbi.AsRef();
-			Assert.That(rpbi.UniqueProcessId.ToInt32(), Is.EqualTo(curProc.Id));
-			// Have to use ToStructure here since PEB has managed types
-			var peb = rpbi.PebBaseAddress.ToStructure<PEB>();
-			// Have to use ToStructure here since RTL_USER_PROCESS_PARAMETERS has managed types
-			var upp = peb.ProcessParameters.ToStructure<RTL_USER_PROCESS_PARAMETERS>();
-			TestContext.WriteLine($"Img: {upp.ImagePathName}; CmdLine: {upp.CommandLine}");
+			using var pbi = NtQueryInformationProcess<PROCESS_BASIC_INFORMATION>(hProc, PROCESSINFOCLASS.ProcessBasicInformation);
+			Assert.That(pbi, ResultIs.ValidHandle);
+			// Can get pointer here since PROCESS_BASIC_INFORMATION has no managed types
+			unsafe
+			{
+				var rpbi = (PROCESS_BASIC_INFORMATION*)pbi;
+				Assert.That(rpbi->UniqueProcessId.ToInt32(), Is.EqualTo(Kernel32.GetCurrentProcessId()));
+				Assert.That(rpbi->PebBaseAddress, Is.Not.EqualTo(IntPtr.Zero));
+				// Have to use ToStructure here since PEB has managed types
+				var peb = rpbi->PebBaseAddress.ToStructure<PEB>();
+				// Have to use ToStructure here since RTL_USER_PROCESS_PARAMETERS has managed types
+				var upp = peb.ProcessParameters.ToStructure<RTL_USER_PROCESS_PARAMETERS>();
+				Assert.That(upp.CommandLine.ToString(hProc), Is.Not.Empty);
+				TestContext.WriteLine($"Img: {upp.ImagePathName.ToString(hProc)}; CmdLine: {upp.CommandLine.ToString(hProc)}");
+			}
 
 			NtQueryResult<IntPtr> pdp = null;
 			Assert.That(() => pdp = NtQueryInformationProcess<IntPtr>(hProc, PROCESSINFOCLASS.ProcessDebugPort), Throws.Nothing);
-			Assert.That(pdp, Is.Not.Null);
+			Assert.That(pdp, ResultIs.ValidHandle);
 			TestContext.WriteLine($"DbgPort: {pdp.Value.ToInt64()}");
 
 			NtQueryResult<BOOL> pwi = null;
 			Assert.That(() => pwi = NtQueryInformationProcess<BOOL>(hProc, PROCESSINFOCLASS.ProcessWow64Information), Throws.Nothing);
-			Assert.That(pwi, Is.Not.Null);
+			Assert.That(pwi, ResultIs.ValidHandle);
 			Assert.That(pwi.Value.Value, Is.True);
 
 			NtQueryResult<UNICODE_STRING> pfn = null;
 			Assert.That(() => pfn = NtQueryInformationProcess<UNICODE_STRING>(hProc, PROCESSINFOCLASS.ProcessImageFileName), Throws.Nothing);
-			Assert.That(pfn, Is.Not.Null);
-			TestContext.WriteLine($"Fn: {pfn}");
+			Assert.That(pfn, ResultIs.ValidHandle);
+			TestContext.WriteLine($"Fn: {pfn.Value.ToString(hProc)}");
 
 			NtQueryResult<BOOL> pbt = null;
 			Assert.That(() => pbt = NtQueryInformationProcess<BOOL>(hProc, PROCESSINFOCLASS.ProcessBreakOnTermination), Throws.Nothing);
-			Assert.That(pbt, Is.Not.Null);
+			Assert.That(pbt, ResultIs.ValidHandle);
 			Assert.That(pbt.Value.Value, Is.False);
 
 			NtQueryResult<SUBSYSTEM_INFORMATION_TYPE> psi = null;
 			// This is documented, but fails on Win10
 			Assert.That(() => psi = NtQueryInformationProcess<SUBSYSTEM_INFORMATION_TYPE>(hProc, PROCESSINFOCLASS.ProcessSubsystemInformation), Throws.ArgumentException);
-			//Assert.That(psi, Is.Not.Null);
+			//Assert.That(psi, ResultIs.ValidHandle);
 			//Assert.That(Enum.IsDefined(typeof(SUBSYSTEM_INFORMATION_TYPE), psi.Value), Is.True);
 			//TestContext.WriteLine($"SubSys: {psi.Value}");
-		}
 
-		[Test]
-		public void GetCommandLineTest()
-		{
-			var randProc = System.Diagnostics.Process.GetProcesses().Where(p => p.ProcessName.StartsWith("devenv")).First();
-			using var hProc = Kernel32.OpenProcess((uint)(Kernel32.ProcessAccess.PROCESS_QUERY_INFORMATION | Kernel32.ProcessAccess.PROCESS_VM_READ), false, (uint)randProc.Id);
-			Assert.That(hProc, ResultIs.ValidHandle);
-
-			NtQueryResult<PROCESS_BASIC_INFORMATION> info = null;
-			Assert.That(() => info = NtQueryInformationProcess<PROCESS_BASIC_INFORMATION>(hProc, PROCESSINFOCLASS.ProcessBasicInformation), Throws.Nothing);
-			Assert.That(info, Is.Not.Null);
-			Assert.That(info.AsRef().PebBaseAddress, Is.Not.EqualTo(IntPtr.Zero));
-
-			using var pebPtr = new SafeHGlobalStruct<PEB>();
-			Assert.That(Kernel32.ReadProcessMemory(hProc, info.AsRef().PebBaseAddress, pebPtr, pebPtr.Size, out var pebSzRead), ResultIs.Successful);
-			Assert.That(pebSzRead, Is.LessThanOrEqualTo(pebPtr.Size));
-
-			using var rtlUserParamsPtr = new SafeHGlobalStruct<RTL_USER_PROCESS_PARAMETERS>();
-			Assert.That(Kernel32.ReadProcessMemory(hProc, pebPtr.Value.ProcessParameters, rtlUserParamsPtr, rtlUserParamsPtr.Size, out var rtlUserParamsRead), ResultIs.Successful);
-			Assert.That(rtlUserParamsRead, Is.LessThanOrEqualTo(rtlUserParamsPtr.Size));
-			var rtlUser = rtlUserParamsPtr.Value;
-			Assert.That(rtlUser.ImagePathName.Length, Is.GreaterThan(0));
-
-			var sImage = GetString(rtlUser.ImagePathName);
-			StringAssert.StartsWith("C:\\", sImage);
-			TestContext.WriteLine($"Img: {sImage}; CmdLine: {GetString(rtlUser.CommandLine)}");
-
-			string GetString(in UNICODE_STRING us)
-			{
-				using var mem = new SafeCoTaskMemString(us.MaximumLength);
-				Assert.That(Kernel32.ReadProcessMemory(hProc, us.Buffer, mem, mem.Size, out _), ResultIs.Successful);
-				return mem;
-			}
+			// Try undocumented fetch
+			NtQueryResult<uint> ppb = null;
+			Assert.That(() => ppb = NtQueryInformationProcess<uint>(hProc, PROCESSINFOCLASS.ProcessPriorityBoost), Throws.Nothing);
+			TestContext.WriteLine($"Priority boost: {ppb.Value}");
 		}
 	}
 }
