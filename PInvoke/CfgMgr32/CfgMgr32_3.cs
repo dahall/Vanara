058dbc456860b7559b7fbca6b4fda1d2c9c295cc
dahall <dahall@byu.net>
2021-04-28 21:15:22 -0600

    More functions to CfgMgr32

---
+++ PInvoke/CfgMgr32/CfgMgr32_3.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using static Vanara.PInvoke.SetupAPI;
+
+namespace Vanara.PInvoke
+{
+	/// <summary>Items from the CfgMgr32.dll</summary>
+	public static partial class CfgMgr32
+	{
+		/*
+		CM_Get_Sibling
+		CM_Get_Sibling_Ex
+		CM_Get_Version
+		CM_Get_Version_Ex
+		CM_Is_Dock_Station_Present
+		CM_Is_Dock_Station_Present_Ex
+		CM_Is_Version_Available
+		CM_Is_Version_Available_Ex
+		CM_Locate_DevNode_ExW
+		CM_Locate_DevNodeA
+		CM_Locate_DevNodeW
+		CM_MapCrToWin32Err
+		CM_Modify_Res_Des
+		CM_Modify_Res_Des_Ex
+		CM_Open_Class_KeyW
+		CM_Open_Device_Interface_Key_ExA
+		CM_Open_Device_Interface_Key_ExW
+		CM_Open_Device_Interface_KeyA
+		CM_Open_Device_Interface_KeyW
+		CM_Open_DevNode_Key
+		CM_Query_And_Remove_SubTree_ExW
+		CM_Query_And_Remove_SubTreeW
+		CM_Query_Resource_Conflict_List
+		CM_Reenumerate_DevNode
+		CM_Reenumerate_DevNode_Ex
+		CM_Register_Notification
+		CM_Request_Device_Eject_ExW
+		CM_Request_Device_EjectW
+		CM_Request_Eject_PC
+		CM_Request_Eject_PC_Ex
+		CM_Set_Class_Property_ExW
+		CM_Set_Class_PropertyW
+		CM_Set_Class_Registry_PropertyW
+		CM_Set_Device_Interface_Property_ExW
+		CM_Set_Device_Interface_PropertyW
+		CM_Set_DevNode_Problem
+		CM_Set_DevNode_Problem_Ex
+		CM_Set_DevNode_Property_ExW
+		CM_Set_DevNode_PropertyW
+		CM_Set_DevNode_Registry_PropertyW
+		CM_Setup_DevNode
+		CM_Uninstall_DevNode
+		CM_Unregister_Notification
+		CM_WaitNoPendingInstallEvents
+
+		BUSNUMBER_DES
+		BUSNUMBER_RANGE
+		BUSNUMBER_RESOURCE
+		CM_NOTIFY_EVENT_DATA
+		CM_NOTIFY_FILTER
+		CS_DES
+		CS_RESOURCE
+		DMA_DES
+		DMA_RANGE
+		DMA_RESOURCE
+		IO_DES
+		IO_RANGE
+		IO_RESOURCE
+		IRQ_DES_32
+		IRQ_DES_64
+		IRQ_RANGE
+		IRQ_RESOURCE_32
+		IRQ_RESOURCE_64
+		MEM_DES
+		MEM_RANGE
+		MEM_RESOURCE
+		MFCARD_DES
+		MFCARD_RESOURCE
+		PCCARD_DES
+		PCCARD_RESOURCE
+
+		CM_NOTIFY_ACTION
+
+		 */
+	}
+}
