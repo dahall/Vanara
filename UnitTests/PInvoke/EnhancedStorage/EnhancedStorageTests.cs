655b9db40e2c8c716cd2334e71ed14556110006e
dahall <dahall@byu.net>
2021-10-19 18:28:57 -0600

    Added test projects for EhStor and WPD assemblies

---
+++ UnitTests/PInvoke/EnhancedStorage/EnhancedStorageTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
+using System.Runtime.InteropServices;
+using static Vanara.PInvoke.EnhancedStorage;
+
+namespace Vanara.PInvoke.Tests
+{
+    [TestFixture]
+    public class EnhancedStorageTests
+    {
+    }
+}
