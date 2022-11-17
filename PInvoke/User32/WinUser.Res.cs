13b2c2960c8ece66bf2540188bb5bfe3be9f660a
David Hall <dahall@byu.net>
2022-11-16 21:38:31 -0700

    Added OBM_ constants

--- PInvoke/User32/WinUser.Res.cs
+++ PInvoke/User32/WinUser.Res.cs
@@ -7,6 +7,43 @@ namespace Vanara.PInvoke
 {
 	public static partial class User32
 	{
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+		public const int OBM_CLOSE = 32754;
+		public const int OBM_UPARROW = 32753;
+		public const int OBM_DNARROW = 32752;
+		public const int OBM_RGARROW = 32751;
+		public const int OBM_LFARROW = 32750;
+		public const int OBM_REDUCE = 32749;
+		public const int OBM_ZOOM = 32748;
+		public const int OBM_RESTORE = 32747;
+		public const int OBM_REDUCED = 32746;
+		public const int OBM_ZOOMD = 32745;
+		public const int OBM_RESTORED = 32744;
+		public const int OBM_UPARROWD = 32743;
+		public const int OBM_DNARROWD = 32742;
+		public const int OBM_RGARROWD = 32741;
+		public const int OBM_LFARROWD = 32740;
+		public const int OBM_MNARROW = 32739;
+		public const int OBM_COMBO = 32738;
+		public const int OBM_UPARROWI = 32737;
+		public const int OBM_DNARROWI = 32736;
+		public const int OBM_RGARROWI = 32735;
+		public const int OBM_LFARROWI = 32734;
+		public const int OBM_OLD_CLOSE = 32767;
+		public const int OBM_SIZE = 32766;
+		public const int OBM_OLD_UPARROW = 32765;
+		public const int OBM_OLD_DNARROW = 32764;
+		public const int OBM_OLD_RGARROW = 32763;
+		public const int OBM_OLD_LFARROW = 32762;
+		public const int OBM_BTSIZE = 32761;
+		public const int OBM_CHECK = 32760;
+		public const int OBM_CHECKBOXES = 32759;
+		public const int OBM_BTNCORNERS = 32758;
+		public const int OBM_OLD_REDUCE = 32757;
+		public const int OBM_OLD_ZOOM = 32756;
+		public const int OBM_OLD_RESTORE = 32755;
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+
 		/// <summary>Specifies the load options for <see cref="LoadImage"/>.</summary>
 		[Flags]
 		public enum LoadImageOptions : uint
