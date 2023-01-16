bebc771ac69242f0a269fa696fd0faac6b91bbae
David Hall <dahall@byu.net>
2023-01-16 12:06:28 -0700

    Added interface indicating that a specific indexer is supported.

---
+++ Core/ISupportIndexer.cs
@@ -0,0 +1,12 @@
+﻿namespace Vanara;
+
+/// <summary>Interface representing a class that holds an indexer.</summary>
+/// <typeparam name="TVal">The type of the indexer's value.</typeparam>
+/// <typeparam name="TRet">The type of the indexer's return value.</typeparam>
+public interface ISupportIndexer<TVal, TRet>
+{
+	/// <summary>Gets or sets the <typeparamref name="TRet"/> with the specified <typeparamref name="TVal"/>.</summary>
+	/// <value>The <typeparamref name="TRet"/>.</value>
+	/// <returns>The <typeparamref name="TVal"/>.</returns>
+	TRet this[TVal index] { get; set; }
+}
