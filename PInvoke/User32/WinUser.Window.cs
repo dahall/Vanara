85ae1149a74d749262b1c5569b43b78786973b57
David Hall <dahall@byu.net>
2023-02-21 21:34:20 -0700

    Added CorrespondingTypeAttribute values to GetClassLongFlag enum

--- PInvoke/User32/WinUser.Window.cs
+++ PInvoke/User32/WinUser.Window.cs
@@ -329,69 +329,87 @@ namespace Vanara.PInvoke
 		public enum GetClassLongFlag
 		{
 			/// <summary>Retrieves the address of the menu name string. The string identifies the menu resource associated with the class.</summary>
+			[CorrespondingType(typeof(StrPtrAuto))]
 			GCLP_MENUNAME = -8,
 
 			/// <summary>Retrieves a handle to the background brush associated with the class.</summary>
+			[CorrespondingType(typeof(HBRUSH))]
 			GCLP_HBRBACKGROUND = -10,
 
 			/// <summary>Retrieves a handle to the cursor associated with the class.</summary>
+			[CorrespondingType(typeof(HCURSOR))]
 			GCLP_HCURSOR = -12,
 
 			/// <summary>Retrieves a handle to the icon associated with the class.</summary>
+			[CorrespondingType(typeof(HICON))]
 			GCLP_HICON = -14,
 
 			/// <summary>Retrieves a handle to the module that registered the class.</summary>
+			[CorrespondingType(typeof(HINSTANCE))]
 			GCLP_HMODULE = -16,
 
 			/// <summary>
 			/// Retrieves the address of the window procedure, or a handle representing the address of the window procedure. You must use the
 			/// CallWindowProc function to call the window procedure.
 			/// </summary>
+			[CorrespondingType(typeof(IntPtr))]
 			GCLP_WNDPROC = -24,
 
 			/// <summary>Retrieves a handle to the small icon associated with the class.</summary>
+			[CorrespondingType(typeof(HICON))]
 			GCLP_HICONSM = -34,
 
 			/// <summary>
 			/// Retrieves an ATOM value that uniquely identifies the window class. This is the same atom that the RegisterClass or
 			/// RegisterClassEx function returns.
 			/// </summary>
+			[CorrespondingType(typeof(ushort))]
 			GCW_ATOM = -32,
 
 			/// <summary>Retrieves the address of the menu name string. The string identifies the menu resource associated with the class.</summary>
+			[CorrespondingType(typeof(StrPtrAuto))]
 			GCL_MENUNAME = -8,
 
 			/// <summary>Retrieves a handle to the background brush associated with the class.</summary>
+			[CorrespondingType(typeof(HBRUSH))]
 			GCL_HBRBACKGROUND = -10,
 
 			/// <summary>Retrieves a handle to the cursor associated with the class.</summary>
+			[CorrespondingType(typeof(HCURSOR))]
 			GCL_HCURSOR = -12,
 
 			/// <summary>Retrieves a handle to the icon associated with the class.</summary>
+			[CorrespondingType(typeof(HICON))]
 			GCL_HICON = -14,
 
 			/// <summary>Retrieves a handle to the module that registered the class.</summary>
+			[CorrespondingType(typeof(HINSTANCE))]
 			GCL_HMODULE = -16,
 
 			/// <summary>
 			/// Retrieves the size, in bytes, of the extra window memory associated with each window in the class. For information on how to
 			/// access this memory, see GetWindowLong.
 			/// </summary>
+			[CorrespondingType(typeof(uint))]
 			GCL_CBWNDEXTRA = -18,
 
 			/// <summary>Retrieves the size, in bytes, of the extra memory associated with the class.</summary>
+			[CorrespondingType(typeof(uint))]
 			GCL_CBCLSEXTRA = -20,
 
 			/// <summary>
 			/// Retrieves the address of the window procedure, or a handle representing the address of the window procedure. You must use the
 			/// CallWindowProc function to call the window procedure.
 			/// </summary>
+			[CorrespondingType(typeof(IntPtr))]
 			GCL_WNDPROC = -24,
 
 			/// <summary>Retrieves the window-class style bits.</summary>
+			[CorrespondingType(typeof(WindowClassStyles))]
 			GCL_STYLE = -26,
 
 			/// <summary>Retrieves a handle to the small icon associated with the class.</summary>
+			[CorrespondingType(typeof(HICON))]
 			GCL_HICONSM = -34,
 		}
 
