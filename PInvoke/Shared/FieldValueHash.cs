b3e390b7099964ea92482360b3e5cdc3d4abc1ec
David Hall <dahall@byu.net>
2024-04-27 12:52:47 -0600

    Added IConvertible to error base type in FieldValueHash

--- PInvoke/Shared/FieldValueHash.cs
+++ PInvoke/Shared/FieldValueHash.cs
@@ -6,9 +6,9 @@ using System.Reflection;
 namespace Vanara.PInvoke;
 
 /// <summary>Gets a static field's name from its value and caches the list for faster lookups.</summary>
-public class StaticFieldValueHash
+public static class StaticFieldValueHash
 {
-	private static readonly Dictionary<(Type, Type), IDictionary<int, (string, string?)>> cache = new();
+	private static readonly Dictionary<(Type, Type), IDictionary<int, (string, string?)>> cache = [];
 
 	/// <summary>Adds the seqence of field values to the associated cache.</summary>
 	/// <typeparam name="TType">The type of the type.</typeparam>
@@ -16,7 +16,7 @@ public class StaticFieldValueHash
 	/// <param name="fields">The list of field values and names to add.</param>
 	/// <param name="lib">The optional library name.</param>
 	public static void AddFields<TType, TFieldType>(IEnumerable<(TFieldType value, string name)> fields, string? lib = null)
-		where TFieldType : struct, IComparable
+		where TFieldType : struct, IComparable, IConvertible
 	{
 		TryGetFieldName<TType, TFieldType>(default, out _); // Load default values
 		var tt = (typeof(TType), typeof(TFieldType));
@@ -33,7 +33,7 @@ public class StaticFieldValueHash
 	/// <typeparam name="TFieldType">The type of the field type.</typeparam>
 	/// <typeparam name="TEnum">The type of the enum to added.</typeparam>
 	/// <param name="lib">The optional library name.</param>
-	public static void AddFields<TType, TFieldType, TEnum>(string? lib = null) where TFieldType : struct, IComparable =>
+	public static void AddFields<TType, TFieldType, TEnum>(string? lib = null) where TFieldType : struct, IComparable, IConvertible =>
 		AddFields<TType, TFieldType>(Enum.GetValues(typeof(TEnum)).Cast<TFieldType>().Select(v => (v, Enum.GetName(typeof(TEnum), v)!)), lib);
 
 	/// <summary>Tries to get the name of a value's library.</summary>
@@ -42,7 +42,7 @@ public class StaticFieldValueHash
 	/// <param name="value">The value for which to search.</param>
 	/// <returns>The name of the library or <see langword="null"/>.</returns>
 	public static string? GetFieldLib<TType, TFieldType>(TFieldType value)
-		where TFieldType : struct, IComparable
+		where TFieldType : struct, IComparable, IConvertible
 	{
 		var tt = (typeof(TType), typeof(TFieldType));
 		return cache.TryGetValue(tt, out var hash) && hash.TryGetValue(value.GetHashCode(), out var t) ? t.Item2 : null;
@@ -55,7 +55,7 @@ public class StaticFieldValueHash
 	/// <param name="fieldName">On success, the name of the field.</param>
 	/// <returns><see langword="true"/> if the value was found, otherwise <see langword="false"/>.</returns>
 	public static bool TryGetFieldName<TType, TFieldType>(TFieldType value, [NotNullWhen(true)] out string? fieldName)
-		where TFieldType : struct, IComparable
+		where TFieldType : struct, IComparable, IConvertible
 	{
 		var tt = (typeof(TType), typeof(TFieldType));
 		if (!cache.TryGetValue(tt, out var hash))
@@ -70,7 +70,7 @@ public class StaticFieldValueHash
 		return ret;
 	}
 
-	private class FIValueComp<TFieldType> : IEqualityComparer<FieldInfo> where TFieldType : struct, IComparable
+	private class FIValueComp<TFieldType> : IEqualityComparer<FieldInfo> where TFieldType : struct, IComparable, IConvertible
 	{
 		bool IEqualityComparer<FieldInfo>.Equals(FieldInfo? x, FieldInfo? y) =>
 			Comparer<TFieldType?>.Default.Compare((TFieldType?)x?.GetValue(null), (TFieldType?)y?.GetValue(null)) == 0;
