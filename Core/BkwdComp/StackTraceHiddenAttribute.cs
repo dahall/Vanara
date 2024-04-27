01c31c3638f2000cdbfa18bd9040eb5cdcd457ab
David Hall <dahall@byu.net>
2024-04-27 12:51:14 -0600

    Added backwards compatible StackTraceHiddenAttribute for pre-NET 6.0

---
+++ Core/BkwdComp/StackTraceHiddenAttribute.cs
@@ -0,0 +1,13 @@
+﻿#if !(NET6_0_OR_GREATER)
+namespace System.Diagnostics;
+
+/// <summary>
+/// Types and Methods attributed with StackTraceHidden will be omitted from the stack trace text shown in StackTrace.ToString() and Exception.StackTrace
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Constructor | AttributeTargets.Method, Inherited = false)]
+public sealed class StackTraceHiddenAttribute : Attribute
+{
+	/// <summary>Initializes a new instance of the <see cref="StackTraceHiddenAttribute"/> class.</summary>
+	public StackTraceHiddenAttribute() { }
+}
+#endif
