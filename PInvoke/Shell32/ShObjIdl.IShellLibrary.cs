f95000a6e660a3795769d8119a3a6b78b757cc79
David Hall <dahall@byu.net>
2024-06-15 15:42:28 -0400

    Added a number of missing IShellLibrary macros.

--- PInvoke/Shell32/ShObjIdl.IShellLibrary.cs
+++ PInvoke/Shell32/ShObjIdl.IShellLibrary.cs
@@ -110,8 +110,8 @@ public static partial class Shell32
 		/// located before the specified time elapses, an error is returned.
 		/// </param>
 		/// <param name="riid">
-		/// A reference to the IID of the interface to get in ppv that will represent the resolved target location. This value is
-		/// typically IID_IShellItem, but it can also be IID_IShellItem2 or the IID of any other interface that is implemented by CShellItem.
+		/// A reference to the IID of the interface to get in ppv that will represent the resolved target location. This value is typically
+		/// IID_IShellItem, but it can also be IID_IShellItem2 or the IID of any other interface that is implemented by CShellItem.
 		/// </param>
 		/// <returns>A pointer to the interface requested in riid.</returns>
 		[return: MarshalAs(UnmanagedType.Interface)]
@@ -130,23 +130,22 @@ public static partial class Shell32
 		/// <summary>Sets the default target folder that the library will use for save operations.</summary>
 		/// <param name="dsft">The DEFAULTSAVEFOLDERTYPE value that specifies the default save location to set.</param>
 		/// <param name="si">
-		/// An IShellItem object that represents the folder that to use as the default save location. The folder that this object
-		/// represents must be a folder that is already in the library.
+		/// An IShellItem object that represents the folder that to use as the default save location. The folder that this object represents
+		/// must be a folder that is already in the library.
 		/// </param>
 		void SetDefaultSaveFolder([In] DEFAULTSAVEFOLDERTYPE dsft, [In] IShellItem si);
 
 		/// <summary>Gets the library's options.</summary>
 		/// <returns>
-		/// The library options for this library. LIBRARYOPTIONFLAGS is a bitwise enumerator, which means that more than one flag could
-		/// be set.
+		/// The library options for this library. LIBRARYOPTIONFLAGS is a bitwise enumerator, which means that more than one flag could be set.
 		/// </returns>
 		LIBRARYOPTIONFLAGS GetOptions();
 
 		/// <summary>Sets the library options.</summary>
 		/// <param name="lofMask">A bitmask that specifies the LIBRARYOPTIONFLAGS values to change in this call.</param>
 		/// <param name="lofOptions">
-		/// A bitmask that specifies the new value of each LIBRARYOPTIONFLAGS value to change. LIBRARYOPTIONFLAGS values that are not set
-		/// in lofMask are not changed by this call.
+		/// A bitmask that specifies the new value of each LIBRARYOPTIONFLAGS value to change. LIBRARYOPTIONFLAGS values that are not set in
+		/// lofMask are not changed by this call.
 		/// </param>
 		void SetOptions([In] LIBRARYOPTIONFLAGS lofMask, [In] LIBRARYOPTIONFLAGS lofOptions);
 
@@ -181,12 +180,12 @@ public static partial class Shell32
 
 		/// <summary>Saves the library to a new Library Description (*.library-ms) file.</summary>
 		/// <param name="folderToSaveIn">
-		/// The IShellItem object that specifies the folder in which to save the library, or NULL to save the library with the user's
-		/// default libraries in the FOLDERID_Libraries known folder.
+		/// The IShellItem object that specifies the folder in which to save the library, or NULL to save the library with the user's default
+		/// libraries in the FOLDERID_Libraries known folder.
 		/// </param>
 		/// <param name="libraryName">
-		/// The file name under which to save the library. The file name must not include the file name extension; the file name
-		/// extension is added automatically.
+		/// The file name under which to save the library. The file name must not include the file name extension; the file name extension is
+		/// added automatically.
 		/// </param>
 		/// <param name="lsf">The LIBRARYSAVEFLAGS value that specifies how to handle a library name collision.</param>
 		/// <returns>The IShellItem object that represents the library description file into which the library was saved.</returns>
@@ -194,12 +193,10 @@ public static partial class Shell32
 		IShellItem Save([In] IShellItem? folderToSaveIn, [In, MarshalAs(UnmanagedType.LPWStr)] string libraryName, [In] LIBRARYSAVEFLAGS lsf);
 
 		/// <summary>Saves the library to a new file in a specified known folder.</summary>
-		/// <param name="kfidToSaveIn">
-		/// The ID of the known folder in which to save the IShellLibrary object. For more information, see KNOWNFOLDERID.
-		/// </param>
+		/// <param name="kfidToSaveIn">The ID of the known folder in which to save the IShellLibrary object. For more information, see KNOWNFOLDERID.</param>
 		/// <param name="libraryName">
-		/// The file name under which to save the library. The file name must not include the file name extension; the file name
-		/// extension is added automatically.
+		/// The file name under which to save the library. The file name must not include the file name extension; the file name extension is
+		/// added automatically.
 		/// </param>
 		/// <param name="lsf">The LIBRARYSAVEFLAGS value that specifies how to handle a library name collision.</param>
 		/// <returns>The IShellItem object that represents the library description file into which the library was saved.</returns>
@@ -226,21 +223,168 @@ public static partial class Shell32
 	/// <param name="sl">The <see cref="IShellLibrary"/> instance.</param>
 	/// <param name="folderToResolve">An IShellItem object that represents the library folder to locate.</param>
 	/// <param name="timeout">
-	/// The maximum time, in milliseconds, the method will attempt to locate the folder before returning. If the folder could not be
-	/// located before the specified time elapses, an error is returned.
+	/// The maximum time, in milliseconds, the method will attempt to locate the folder before returning. If the folder could not be located
+	/// before the specified time elapses, an error is returned.
 	/// </param>
 	/// <returns>A pointer to the interface requested.</returns>
 	public static T ResolveFolder<T>(this IShellLibrary sl, [In] IShellItem folderToResolve, [In] uint timeout) where T : class => (T)sl.ResolveFolder(folderToResolve, timeout, typeof(T).GUID);
+
+	/// <summary>Creates an IShellLibrary object.</summary>
+	/// <param name="riid">
+	/// <para>Type: <c>REFIID</c></para>
+	/// <para>The IID for IShellLibrary. (See usage remarks.)</para>
+	/// </param>
+	/// <param name="ppv">
+	/// <para>Type: <c>void**</c></para>
+	/// <para>Receives a new IShellLibrary object. (See usage remarks.)</para>
+	/// </param>
+	/// <returns>
+	/// <para>Type: <c>HRESULT</c></para>
+	/// <para>If this function succeeds, it returns <c>S_OK</c>. Otherwise, it returns an <c>HRESULT</c> error code.</para>
+	/// </returns>
+	/// <remarks>
+	/// <para>Usage</para>
+	/// <para>
+	/// The <c>IID_PPV_ARGS</c> macro is generally used to generate the <c>riid</c> and <c>ppv</c> parameters for this function. For example:
+	/// </para>
+	/// </remarks>
+	// https://learn.microsoft.com/en-us/windows/win32/api/shobjidl_core/nf-shobjidl_core-shcreatelibrary HRESULT SHCreateLibrary( [in]
+	// REFIID riid, [out] void **ppv );
+	[PInvokeData("shobjidl_core.h", MSDNShortId = "NF:shobjidl_core.SHCreateLibrary")]
+	public static HRESULT SHCreateLibrary(in Guid riid, [MarshalAs(UnmanagedType.IUnknown)] out object? ppv) => SHCreateLibrary(riid, out ppv, null);
+
+	/// <summary>Creates and loads an IShellLibrary object from a specified library definition file.</summary>
+	/// <param name="psiLibrary">
+	/// <para>Type: <c>IShellItem*</c></para>
+	/// <para>An IShellItem object for the library definition file to load.</para>
+	/// </param>
+	/// <param name="grfMode">
+	/// <para>Type: <c>DWORD</c></para>
+	/// <para>
+	/// One or more storage medium flags that specify access and sharing modes for the library object. Commonly specified flags are
+	/// <c>STGM_READ</c> or <c>STGM_READWRITE</c>. For more information, see STGM.
+	/// </para>
+	/// </param>
+	/// <param name="riid">
+	/// <para>Type: <c>REFIID</c></para>
+	/// <para>The IID for IShellLibrary. (See usage remarks.)</para>
+	/// </param>
+	/// <param name="ppv">
+	/// <para>Type: <c>void**</c></para>
+	/// <para>Receives the loaded IShellLibrary object. (See usage remarks.)</para>
+	/// </param>
+	/// <returns>
+	/// <para>Type: <c>HRESULT</c></para>
+	/// <para>If this function succeeds, it returns <c>S_OK</c>. Otherwise, it returns an <c>HRESULT</c> error code.</para>
+	/// </returns>
+	/// <remarks>
+	/// <para>Usage</para>
+	/// <para>
+	/// The <c>IID_PPV_ARGS</c> macro is generally used to generate the <c>riid</c> and <c>ppv</c> parameters for this function. For an
+	/// example, see SHCreateLibrary.
+	/// </para>
+	/// <para>This is an inline helper function that wraps the IShellLibrary::LoadLibraryFromItem method.</para>
+	/// </remarks>
+	// https://learn.microsoft.com/en-us/windows/win32/api/shobjidl_core/nf-shobjidl_core-shloadlibraryfromitem HRESULT
+	// SHLoadLibraryFromItem( [in] IShellItem *psiLibrary, [in] DWORD grfMode, [in] REFIID riid, [out] void **ppv );
+	[PInvokeData("shobjidl_core.h", MSDNShortId = "NF:shobjidl_core.SHLoadLibraryFromItem")]
+	public static HRESULT SHLoadLibraryFromItem([In] IShellItem psiLibrary, STGM grfMode, in Guid riid,
+		[MarshalAs(UnmanagedType.IUnknown)] out object? ppv) => SHCreateLibrary(riid, out ppv, l =>
+		l.LoadLibraryFromItem(psiLibrary, grfMode));
+
+	/// <summary>Creates and loads an IShellLibrary object for a specified known folder ID.</summary>
+	/// <param name="kfidLibrary">
+	/// <para>Type: <c>REFKNOWNFOLDERID</c></para>
+	/// <para>The KNOWNFOLDERID value that identifies the known folder to load into the IShellLibrary object.</para>
+	/// </param>
+	/// <param name="grfMode">
+	/// <para>Type: <c>DWORD</c></para>
+	/// <para>
+	/// One or more storage medium flags that specify access and sharing modes for the library object. Commonly specified flags are
+	/// <c>STGM_READ</c> or <c>STGM_READWRITE</c>. For more information, see STGM.
+	/// </para>
+	/// </param>
+	/// <param name="riid">
+	/// <para>Type: <c>REFIID</c></para>
+	/// <para>The IID for IShellLibrary. (See Remarks for more information.)</para>
+	/// </param>
+	/// <param name="ppv">
+	/// <para>Type: <c>void**</c></para>
+	/// <para>When this function returns successfully, receives the loaded IShellLibrary object. (See Remarks for more information.)</para>
+	/// </param>
+	/// <returns>
+	/// <para>Type: <c>HRESULT</c></para>
+	/// <para>If this function succeeds, it returns <c>S_OK</c>. Otherwise, it returns an <c>HRESULT</c> error code.</para>
+	/// </returns>
+	/// <remarks>
+	/// <para>This is an inline helper function that wraps the IShellLibrary::LoadLibraryFromKnownFolder method.</para>
+	/// <para>Usage</para>
+	/// <para>
+	/// The <c>IID_PPV_ARGS</c> macro is generally used to generate the <c>riid</c> and <c>ppv</c> parameters for this function. For an
+	/// example, see SHCreateLibrary.
+	/// </para>
+	/// </remarks>
+	// https://learn.microsoft.com/en-us/windows/win32/api/shobjidl_core/nf-shobjidl_core-shloadlibraryfromknownfolder HRESULT
+	// SHLoadLibraryFromKnownFolder( [in] REFKNOWNFOLDERID kfidLibrary, [in] DWORD grfMode, [in] REFIID riid, [out] void **ppv );
+	[PInvokeData("shobjidl_core.h", MSDNShortId = "NF:shobjidl_core.SHLoadLibraryFromKnownFolder")]
+	public static HRESULT SHLoadLibraryFromKnownFolder([In] KNOWNFOLDERID kfidLibrary, STGM grfMode, in Guid riid,
+		[MarshalAs(UnmanagedType.IUnknown)] out object? ppv) => SHCreateLibrary(riid, out ppv, l =>
+		l.LoadLibraryFromKnownFolder(kfidLibrary.Guid(), grfMode));
+
+	/// <summary>Creates and loads an IShellLibrary object for a specified path.</summary>
+	/// <param name="pszParsingName">
+	/// <para>Type: <c>PCWSTR</c></para>
+	/// <para>The path for which to load the IShellLibrary object.</para>
+	/// </param>
+	/// <param name="grfMode">
+	/// <para>Type: <c>DWORD</c></para>
+	/// <para>
+	/// One or more storage medium flags that specify access and sharing modes for the library object. Commonly specified flags are
+	/// <c>STGM_READ</c> or <c>STGM_READWRITE</c>. For more information, see STGM.
+	/// </para>
+	/// </param>
+	/// <param name="riid">
+	/// <para>Type: <c>REFIID</c></para>
+	/// <para>A reference to the IID of the interface to retrieve through <c>ppv</c>, typically IID_IShellLibrary.</para>
+	/// </param>
+	/// <param name="ppv">
+	/// <para>Type: <c>void**</c></para>
+	/// <para>When this method returns successfully, contains the interface pointer requested in <c>riid</c>. This is typically IShellLibrary.</para>
+	/// </param>
+	/// <returns>
+	/// <para>Type: <c>HRESULT</c></para>
+	/// <para>If this function succeeds, it returns <c>S_OK</c>. Otherwise, it returns an <c>HRESULT</c> error code.</para>
+	/// </returns>
+	/// <remarks>
+	/// <para>
+	/// We recommend that you use the <c>IID_PPV_ARGS</c> macro, defined in Objbase.h, to package the <c>riid</c> and <c>ppv</c> parameters.
+	/// This macro provides the correct IID based on the interface pointed to by the value in <c>ppv</c>, which eliminates the possibility of
+	/// a coding error in <c>riid</c> that could lead to unexpected results.
+	/// </para>
+	/// <para>This is an inline helper function that wraps the IShellLibrary::LoadLibraryFromItem method.</para>
+	/// </remarks>
+	// https://learn.microsoft.com/en-us/windows/win32/api/shobjidl_core/nf-shobjidl_core-shloadlibraryfromparsingname HRESULT
+	// SHLoadLibraryFromParsingName( [in] PCWSTR pszParsingName, [in] DWORD grfMode, [in] REFIID riid, [out] void **ppv );
+	[PInvokeData("shobjidl_core.h", MSDNShortId = "NF:shobjidl_core.SHLoadLibraryFromParsingName")]
+	public static HRESULT SHLoadLibraryFromParsingName([MarshalAs(UnmanagedType.LPWStr)] string pszParsingName,
+		STGM grfMode, in Guid riid, [MarshalAs(UnmanagedType.IUnknown)] out object? ppv)
+	{
+		ppv = null;
+		var hr = SHCreateItemFromParsingName(pszParsingName, null, typeof(IShellItem).GUID, out var ppsi);
+		if (hr.Succeeded)
+			hr = SHLoadLibraryFromItem((IShellItem)ppsi!, grfMode, riid, out ppv);
+		return hr;
+	}
 
 	/// <summary>Resolves all locations in a library, even those locations that have been moved or renamed.</summary>
 	/// <param name="psiLibrary">A pointer to an IShellItem object that represents the library.</param>
 	/// <returns>If this function succeeds, it returns S_OK. Otherwise, it returns an HRESULT error code.</returns>
 	/// <remarks>
-	/// This function can block the calling thread for as long as it takes to resolve all the locations in the specified library. Because
-	/// it blocks the thread from which it is called, it should not be called from a thread that also handles user interface interactions.
+	/// This function can block the calling thread for as long as it takes to resolve all the locations in the specified library. Because it
+	/// blocks the thread from which it is called, it should not be called from a thread that also handles user interface interactions.
 	/// <para>
-	/// This function resolves all locations in the specified library in a single call. To resolve an individual location in a library,
-	/// see the IShellLibrary::ResolveFolder method or the SHResolveFolderPathInLibrary function.
+	/// This function resolves all locations in the specified library in a single call. To resolve an individual location in a library, see
+	/// the IShellLibrary::ResolveFolder method or the SHResolveFolderPathInLibrary function.
 	/// </para>
 	/// </remarks>
 	[DllImport(Lib.Shell32, ExactSpelling = true)]
@@ -267,6 +411,18 @@ public static partial class Shell32
 	[PInvokeData("Shobjidl.h", MSDNShortId = "dd378433")]
 	public static extern HRESULT SHShowManageLibraryUI(IShellItem psiLibrary, [Optional] HWND hwndOwner, [In, Optional, MarshalAs(UnmanagedType.LPWStr)] string? pszTitle,
 		[In, Optional, MarshalAs(UnmanagedType.LPWStr)] string? pszInstruction, LIBRARYMANAGEDIALOGOPTIONS lmdOptions);
+
+	private static HRESULT SHCreateLibrary(in Guid riid, [MarshalAs(UnmanagedType.IUnknown)] out object? ppv, Action<IShellLibrary>? action)
+	{
+		IShellLibrary l = new();
+		try
+		{
+			action?.Invoke(l);
+			return l.QueryInterface(riid, out ppv);
+		}
+		catch (Exception ex) { ppv = null; return ex.HResult; }
+		finally { Marshal.ReleaseComObject(l); }
+	}
 
 	/// <summary>Class interface for IShellLibrary</summary>
 	[ComImport, Guid("d9b3211d-e57f-4426-aaef-30a806add397"), ClassInterface(ClassInterfaceType.None)]
