05c4b81e19bdf183412fe38bdc0b57d25146376e
David Hall <dahall@byu.net>
2024-08-03 14:15:45 -0600

    Updated AclUI test to show effective rights tab

--- UnitTests/Windows.Forms/Dialogs/AccessControlEditorTests.cs
+++ UnitTests/Windows.Forms/Dialogs/AccessControlEditorTests.cs
@@ -6,7 +6,7 @@ public class AccessControlEditorTests
 	[Test, RequiresThread(System.Threading.ApartmentState.STA)]
 	public void AccessControlEditorTest()
 	{
-		var dlg = new AccessControlEditorDialog() { ElevationRequired = true, AllowEditOwner = true };
+		var dlg = new AccessControlEditorDialog() { ElevationRequired = true, AllowEditOwner = true, ShowEffectivePermissionsPage = true };
 		//dlg.Flags = SI_OBJECT_INFO_Flags.SI_EDIT_OWNER | SI_OBJECT_INFO_Flags.SI_EDIT_AUDITS | SI_OBJECT_INFO_Flags.SI_ADVANCED | SI_OBJECT_INFO_Flags.SI_RESET | SI_OBJECT_INFO_Flags.SI_EDIT_PROPERTIES | SI_OBJECT_INFO_Flags.SI_EDIT_EFFECTIVE | SI_OBJECT_INFO_Flags.SI_RESET_SACL;
 		//dlg.ResourceType = AccessControlEditorDialog.TaskResourceType; dlg.ObjectName = @"AUScheduledInstall";
 		dlg.Initialize(new System.IO.FileInfo(PInvoke.Tests.TestCaseSources.SmallFile));
