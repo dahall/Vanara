8d815737dab0dc53fc8270c118bb5e41509eca8e
dahall <dahall.dev@gmail.com>
2025-02-19 15:40:14 -0700

    Added attributes for code generation of handles

---
+++ Core/Handles/AutoHandleAttribute.cs
@@ -0,0 +1,27 @@
+﻿namespace Vanara.PInvoke;
+
+/// <summary>
+/// Attribute to apply to simple struct definition that will generate the code for a full HANDLE.
+/// </summary>
+/// <remarks>To use this attribute, apply to a structure that defines the handle name.
+/// <code lang="cs">
+/// /// &lt;summary&gt;A handle to a module.&lt;/summary&gt;
+/// [AutoHandle] // Automatically derives from IHandle
+/// public partial struct HMODULE { }
+/// 
+/// /// &lt;summary&gt;A handle to a synchronization event.&lt;/summary&gt;
+/// [AutoHandle(typeof(ISynchHandle))]
+/// public partial struct HEVENT
+/// {
+///    /// &lt;summary&gt;Sets the signaled state.&lt;/summary&gt;
+///    public bool SetEvent() => SetEvent(handle);
+/// }
+/// </code>
+/// </remarks>
+[AttributeUsage(AttributeTargets.Struct, Inherited = false, AllowMultiple = false)]
+public sealed class AutoHandleAttribute(Type? baseInterface = null) : Attribute
+{
+	/// <summary>Gets or sets the base interface.</summary>
+	/// <value>The base interface.</value>
+	public Type BaseInterface { get; set; } = baseInterface ?? typeof(IHandle);
+}
---
+++ Core/Handles/AutoSafeHandleAttribute.cs
@@ -0,0 +1,33 @@
+﻿namespace Vanara.PInvoke;
+
+/// <summary>
+/// Attribute to apply to simple class definition that will generate the code for a full SafeHANDLE
+/// that will perform a close operation on disposal.
+/// </summary>
+/// <remarks>
+/// To use this attribute, apply to a class that defines the safe handle name.
+/// <code lang="cs">
+/// /// &lt;summary&gt;A safe handle to a module.&lt;/summary&gt;
+/// [AutoSafeHandle(typeof(HMODULE), typeof(SafeHANDLE), "FreeLibrary(handle)")]
+/// public partial class SafeHMODULE { }
+/// </code>
+/// </remarks>
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+public sealed class AutoSafeHandleAttribute(Type handleStruct, Type? baseSafeHandle = null, string? closeHandleFuncName = null) : Attribute
+{
+	/// <summary>Gets the base safe handle. Defaults to <see cref="SafeHandleV"/>.</summary>
+	/// <value>The base safe handle.</value>
+	public Type BaseSafeHandle { get; } = baseSafeHandle ?? typeof(SafeHandleV);
+
+	/// <summary>
+	/// Gets the name of the close handle function. This should be expressed as a lambda function
+	/// that matches the <see cref="CloseHandleFuncName"/> delegate and uses <c>'handle'</c> to
+	/// represent the <see cref="IntPtr"/> value passed to the function.
+	/// </summary>
+	/// <value>The name of the close handle function.</value>
+	public string? CloseHandleFuncName { get; } = closeHandleFuncName;
+
+	/// <summary>Gets the handle type that this safe handle holds and disposes.</summary>
+	/// <value>The handle structure.</value>
+	public Type HandleStruct { get; } = handleStruct;
+}
