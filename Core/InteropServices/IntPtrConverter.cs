55341842e6cdf37bec1f76f48e63196f680e9736
dahall <dahall.dev@gmail.com>
2025-05-06 07:54:25 -0600

    Added Encoding variants to IntPtr.Convert extensions

--- Core/InteropServices/IntPtrConverter.cs
+++ Core/InteropServices/IntPtrConverter.cs
@@ -9,18 +9,34 @@ public static class IntPtrConverter
 	/// <param name="sz">The size of the allocated memory block.</param>
 	/// <param name="charSet">The character set.</param>
 	/// <returns>A value of the type specified.</returns>
-	public static T? Convert<T>(this IntPtr ptr, uint sz, CharSet charSet = CharSet.Auto) => (T?)Convert(ptr, sz, typeof(T), charSet);
+	public static T? Convert<T>(this IntPtr ptr, uint sz, CharSet charSet = CharSet.Auto) => Convert<T>(ptr, sz, charSet.ToEncoding());
+
+	/// <summary>Converts the specified pointer to <typeparamref name="T"/>.</summary>
+	/// <typeparam name="T">The destination type.</typeparam>
+	/// <param name="ptr">The pointer to a block of memory.</param>
+	/// <param name="sz">The size of the allocated memory block.</param>
+	/// <param name="encoding">The character encoding.</param>
+	/// <returns>A value of the type specified.</returns>
+	public static T? Convert<T>(this IntPtr ptr, uint sz, Encoding encoding) => (T?)Convert(ptr, sz, typeof(T), encoding);
 
 	/// <summary>Converts the specified pointer to <typeparamref name="T"/>.</summary>
 	/// <typeparam name="T">The destination type.</typeparam>
 	/// <param name="hMem">A block of allocated memory.</param>
 	/// <param name="charSet">The character set.</param>
 	/// <returns>A value of the type specified.</returns>
-	public static T? ToType<T>(this SafeAllocatedMemoryHandle hMem, CharSet charSet = CharSet.Auto)
+	public static T? ToType<T>(this SafeAllocatedMemoryHandle hMem, CharSet charSet = CharSet.Auto) =>
+		ToType<T>(hMem, charSet.ToEncoding());
+
+	/// <summary>Converts the specified pointer to <typeparamref name="T"/>.</summary>
+	/// <typeparam name="T">The destination type.</typeparam>
+	/// <param name="hMem">A block of allocated memory.</param>
+	/// <param name="encoding">The character encoding.</param>
+	/// <returns>A value of the type specified.</returns>
+	public static T? ToType<T>(this SafeAllocatedMemoryHandle hMem, Encoding encoding)
 	{
 		if (hMem is null) throw new ArgumentNullException(nameof(hMem));
 		hMem.Lock();
-		try { return Convert<T>(hMem.DangerousGetHandle(), hMem.Size, charSet); }
+		try { return Convert<T>(hMem.DangerousGetHandle(), hMem.Size, encoding); }
 		finally { hMem.Unlock(); }
 	}
 
@@ -33,7 +49,18 @@ public static class IntPtrConverter
 	/// <exception cref="ArgumentException">Cannot convert a null pointer. - ptr or Cannot convert a pointer with no Size. - sz</exception>
 	/// <exception cref="NotSupportedException">Thrown if type cannot be converted from memory.</exception>
 	/// <exception cref="OutOfMemoryException"></exception>
-	public static object? Convert(this IntPtr ptr, uint sz, Type destType, CharSet charSet = CharSet.Auto)
+	public static object? Convert(this IntPtr ptr, uint sz, Type destType, CharSet charSet = CharSet.Auto) => Convert(ptr, sz, destType, charSet.ToEncoding());
+
+	/// <summary>Converts the specified pointer to type specified in <paramref name="destType"/>.</summary>
+	/// <param name="ptr">The pointer to a block of memory.</param>
+	/// <param name="sz">The size of the allocated memory block.</param>
+	/// <param name="destType">The destination type.</param>
+	/// <param name="encoding">The character encoding.</param>
+	/// <returns>A value of the type specified.</returns>
+	/// <exception cref="ArgumentException">Cannot convert a null pointer. - ptr or Cannot convert a pointer with no Size. - sz</exception>
+	/// <exception cref="NotSupportedException">Thrown if type cannot be converted from memory.</exception>
+	/// <exception cref="OutOfMemoryException"></exception>
+	public static object? Convert(this IntPtr ptr, uint sz, Type destType, Encoding encoding)
 	{
 		if (ptr == IntPtr.Zero)
 		{
@@ -52,7 +79,7 @@ public static class IntPtrConverter
 			return ptr.ToArray(elemType, (int)sz / elemSz, 0, sz);
 		}
 		if (destType == typeof(string))
-			return StringHelper.GetString(ptr, charSet, sz);
+			return StringHelper.GetString(ptr, encoding, out _, sz);
 		if (destType.IsCOMObject)
 			return Marshal.GetObjectForIUnknown(ptr);
 		return ptr.ToStructure(destType, sz, 0, out _);
