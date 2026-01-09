b0635d80bb97442a40939d3c7f36ed3693ac47df
dahall <dahall.dev@gmail.com>
2026-01-08 20:50:32 -0700

    Added EnumRebase struct for enum/base type conversions

---
+++ Core/InteropServices/EnumRebase.cs
@@ -0,0 +1,16 @@
+﻿namespace Vanara.PInvoke;
+
+/// <summary>Helper to convert enum to different underlying base type.</summary>
+/// <typeparam name="TEnum">Enum type.</typeparam>
+/// <typeparam name="T">Underlying type.</typeparam>
+/// <param name="enumVal">Enum value.</param>
+public readonly struct EnumRebase<TEnum, T>(TEnum enumVal) where TEnum : Enum where T : unmanaged, IConvertible
+{
+	private readonly T value = (T)Convert.ChangeType(enumVal, typeof(T));
+
+	/// <summary>Performs an implicit conversion from <typeparamref name="TEnum"/> to <typeparamref name="T"/>.</summary>
+	public static implicit operator EnumRebase<TEnum, T>(TEnum e) => new(e);
+
+	/// <summary>Performs an implicit conversion from <typeparamref name="T"/> to <typeparamref name="TEnum"/>.</summary>
+	public static implicit operator EnumRebase<TEnum, T>(T v) => new((TEnum)Enum.ToObject(typeof(TEnum), v));
+}
