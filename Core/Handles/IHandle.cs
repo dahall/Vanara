4b71b1de1d23332e73a86695b36bf14b938da5b5
dahall <dahall.dev@gmail.com>
2026-01-14 07:40:08 -0700

    Combined IHandle.cs from Shared to Core and project details. Fully removed Shared project files.

--- Core/Handles/IHandle.cs
+++ Core/Handles/IHandle.cs
@@ -11,3 +11,24 @@ public interface IHandle
 	/// <value><see langword="true" /> if this handle is invalid; otherwise, <see langword="false" />.</value>
 	bool IsInvalid { get; }
 }
+
+/// <summary>Signals that a structure or class holds a handle to a device context.</summary>
+public interface IDeviceContextHandle : IHandle { }
+
+/// <summary>Signals that a structure or class holds a handle to a graphics object.</summary>
+public interface IGraphicsObjectHandle : IUserHandle { }
+
+/// <summary>Signals that a structure or class holds a handle to a kernel object.</summary>
+public interface IKernelHandle : IHandle { }
+
+/// <summary>Signals that a structure or class holds a pointer to a security object.</summary>
+public interface ISecurityObject : IHandle { }
+
+/// <summary>Signals that a structure or class holds a handle to a shell object.</summary>
+public interface IShellHandle : IHandle { }
+
+/// <summary>Signals that a structure or class holds a handle to a synchronization object.</summary>
+public interface ISyncHandle : IKernelHandle { }
+
+/// <summary>Signals that a structure or class holds a handle to a user object.</summary>
+public interface IUserHandle : IHandle { }
