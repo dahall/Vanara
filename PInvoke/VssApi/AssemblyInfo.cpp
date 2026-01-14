4aeae6c1a773668755c3eaffecaf3cbdb080556c
dahall <dahall.dev@gmail.com>
2026-01-14 07:14:33 -0700

    Changed version to 5.0.1 and updated C++ build for VssApi

--- PInvoke/VssApi/AssemblyInfo.cpp
+++ PInvoke/VssApi/AssemblyInfo.cpp
@@ -11,7 +11,7 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyProductAttribute(L"Vanara")];
 [assembly:AssemblyCopyrightAttribute(L"Copyright © 2017-2025")];
 [assembly:AssemblyKeyFileAttribute(L"..\\..\\Vanara.snk")]
-[assembly:AssemblyVersionAttribute("5.0.0.0")];
-[assembly:AssemblyInformationalVersionAttribute("5.0.0")];
+[assembly:AssemblyVersionAttribute("5.0.1.0")];
+[assembly:AssemblyInformationalVersionAttribute("5.0.1")];
 
 [assembly:ComVisible(false)];
