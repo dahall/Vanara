281058a054a88ffb3b1c444322dda8b48afce08e
Thorsten Jung <tajbender@users.noreply.github.com>
2026-03-10 20:48:11 +0100

    Initial import of Shell32 Controls

---
+++ WinUI.Extensions/Models/LocalSettingsOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace electrifier.Models;
+
+public class LocalSettingsOptions
+{
+    public string? ApplicationDataFolder
+    {
+        get; set;
+    }
+
+    public string? LocalSettingsFile
+    {
+        get; set;
+    }
+}
