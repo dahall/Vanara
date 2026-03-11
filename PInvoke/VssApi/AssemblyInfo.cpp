ff92f7711e4c6d8f264a60dd33c03fb0ddea1301
dahall <dahall.dev@gmail.com>
2026-03-11 17:50:21 -0600

    Updated version to 5.0.2

--- PInvoke/VssApi/AssemblyInfo.cpp
+++ PInvoke/VssApi/AssemblyInfo.cpp
@@ -11,7 +11,7 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyProductAttribute(L"Vanara")];
 [assembly:AssemblyCopyrightAttribute(L"Copyright © 2017-2025")];
 [assembly:AssemblyKeyFileAttribute(L"..\\..\\Vanara.snk")]
-[assembly:AssemblyVersionAttribute("5.0.1.0")];
-[assembly:AssemblyInformationalVersionAttribute("5.0.1")];
+[assembly:AssemblyVersionAttribute("5.0.2.0")];
+[assembly:AssemblyInformationalVersionAttribute("5.0.2")];
 
 [assembly:ComVisible(false)];
